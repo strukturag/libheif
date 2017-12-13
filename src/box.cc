2855c39665791a408353810ba9146869a8e72c78
Joachim Bauch <bauch@struktur.de>
2017-12-13 10:56:28 +0100

    Include "config.h" if available.

--- src/box.cc
+++ src/box.cc
@@ -17,6 +17,9 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with libde265.  If not, see <http://www.gnu.org/licenses/>.
  */
+#if defined(HAVE_CONFIG_H)
+#include "config.h"
+#endif
 
 #include "box.h"
 
