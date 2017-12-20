4c39ec938b6ec742c19442929c45c5b34458f78a
Dirk Farin <farin@struktur.de>
2017-12-20 19:15:28 +0100

    documentation of decoder plugin API

--- src/heif_decoder_plugin.h
+++ src/heif_decoder_plugin.h
@@ -24,20 +24,36 @@
 
 struct heif_decoder_plugin
 {
+  // Create a new decoder context for decoding an image
   void* (*new_decoder)();
 
+  // Free the decoder context (heif_image can still be used after destruction)
   void (*free_decoder)(void*);
 
+  // Push more data into the decoder. This can be called multiple times.
+  // This may not be called after any decode_*() function has been called.
   void (*push_data)(void* decoder, uint8_t* data,uint32_t size);
 
+
+  // --- After pushing the data into the decoder, exactly one of the decode functions may be called once.
+
+  // Decode data into a full image. All data has to be pushed into the decoder before calling this.
   heif_image* (*decode_all)(void* decoder);
 
+  // Decode only part of the image.
+  // May be useful if the input image is tiled and we only need part of it.
   heif_image* (*decode_partial)(void* decoder,
                                 int x_left, int y_top,
                                 int width, int height);
+
+  // Reset decoder, such that we can feed in new data for another image.
+  // void (*reset_image)(void* decoder);
 };
 
-void heif_register_decoder(uint32_t type, const heif_decoder_plugin*);
+
+void heif_register_decoder(heif_file* heif, uint32_t type, const heif_decoder_plugin*);
+
+// TODO void heif_register_encoder(heif_file* heif, uint32_t type, const heif_encoder_plugin*);
 
 
 #endif
