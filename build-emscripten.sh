4ac1804f2b3f523289a7c121381b6dacb4de916e
Joachim Bauch <bauch@struktur.de>
2017-12-21 10:41:03 +0100

    Update build script for new version of Emscripten.

--- build-emscripten.sh
+++ build-emscripten.sh
@@ -9,26 +9,19 @@ if [ ! -e "Makefile" ]; then
 fi
 emmake make
 
-export TOTAL_MEMORY=8388608
-
-export LIBRARY_FUNCTIONS="[ \
-    'memcpy', \
-    'memset', \
-    'malloc', \
-    'free'
-]"
+export TOTAL_MEMORY=16777216
 
 echo "Running Emscripten..."
 emcc src/.libs/libheif.so \
     --bind \
     -s NO_EXIT_RUNTIME=1 \
     -s TOTAL_MEMORY=${TOTAL_MEMORY} \
-    -s ALLOW_MEMORY_GROWTH=1 \
     -s ASSERTIONS=0 \
     -s INVOKE_RUN=0 \
-    -s PRECISE_I32_MUL=0 \
     -s DISABLE_EXCEPTION_CATCHING=1 \
-    -s DEFAULT_LIBRARY_FUNCS_TO_INCLUDE="${LIBRARY_FUNCTIONS}" \
+    -s USE_CLOSURE_COMPILER=1 \
+    -s LEGACY_VM_SUPPORT=1 \
+    --memory-init-file 0 \
     -O3 \
     -std=c++11 \
     --pre-js pre.js \
