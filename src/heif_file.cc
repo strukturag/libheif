357e264bfe8fe24ecc54663804f991e5601bee6d
Dirk Farin <farin@struktur.de>
2017-12-21 12:51:12 +0100

    parse ImageGrid data

--- src/heif_file.cc
+++ src/heif_file.cc
@@ -37,6 +37,77 @@ extern "C" {
 using namespace heif;
 
 
+
+class ImageGrid
+{
+public:
+  Error parse(const std::vector<uint8_t>& data);
+
+  std::string dump() const;
+
+private:
+  uint16_t m_rows;
+  uint16_t m_columns;
+  uint32_t m_output_width;
+  uint32_t m_output_height;
+};
+
+
+Error ImageGrid::parse(const std::vector<uint8_t>& data)
+{
+  if (data.size() < 8) {
+    return Error(Error::InvalidInput, Error::ParseError);
+  }
+
+  uint8_t version = data[0];
+  uint8_t flags = data[1];
+  int field_size = ((flags & 1) ? 32 : 16);
+
+  m_rows    = data[2] +1;
+  m_columns = data[3] +1;
+
+  if (field_size == 32) {
+    if (data.size() < 12) {
+      return Error(Error::InvalidInput, Error::ParseError);
+    }
+
+    m_output_width = ((data[4] << 24) |
+                      (data[5] << 16) |
+                      (data[6] <<  8) |
+                      (data[7]));
+
+    m_output_height = ((data[ 8] << 24) |
+                       (data[ 9] << 16) |
+                       (data[10] <<  8) |
+                       (data[11]));
+  }
+  else {
+    m_output_width = ((data[4] << 8) |
+                      (data[5]));
+
+    m_output_height = ((data[ 6] << 8) |
+                       (data[ 7]));
+  }
+
+  return Error::OK;
+}
+
+
+std::string ImageGrid::dump() const
+{
+  std::ostringstream sstr;
+
+  sstr << "rows: " << m_rows << "\n"
+       << "columns: " << m_columns << "\n"
+       << "output width: " << m_output_width << "\n"
+       << "output height: " << m_output_height << "\n";
+
+  return sstr.str();
+}
+
+
+
+
 HeifFile::HeifFile()
 {
 }
@@ -383,9 +454,9 @@ Error HeifFile::get_image(uint16_t ID, const struct de265_image** img, std::istr
     }
 
 
-    for (uint8_t d : data) {
-      std::cout << std::hex << ((int)d) << "\n";
-    }
+    ImageGrid grid;
+    grid.parse(data);
+    std::cout << grid.dump();
   }
   else {
     return Error(Error::Unsupported, Error::UnsupportedImageType);
