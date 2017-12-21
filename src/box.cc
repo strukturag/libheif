070090e5b9023004f3d0f1c37639702475f837bb
Dirk Farin <farin@struktur.de>
2017-12-21 11:34:27 +0100

    show version 1,2 fields of iloc box

--- src/box.cc
+++ src/box.cc
@@ -758,13 +758,18 @@ std::string Box_iloc::dump(Indent& indent) const
 
   for (const Item& item : m_items) {
     sstr << indent << "item ID: " << item.item_ID << "\n"
-         << indent<< "  data_reference_index: " << std::hex
+         << indent << "  construction method: " << ((int)item.construction_method) << "\n"
+         << indent << "  data_reference_index: " << std::hex
          << item.data_reference_index << std::dec << "\n"
          << indent << "  base_offset: " << item.base_offset << "\n";
 
     sstr << indent << "  extents: ";
     for (const Extent& extent : item.extents) {
-      sstr << extent.offset << "," << extent.length << " ";
+      sstr << extent.offset << "," << extent.length;
+      if (extent.index != 0) {
+        sstr << ";index=" << extent.index;
+      }
+      sstr << " ";
     }
     sstr << "\n";
   }
