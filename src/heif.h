c21513238277607608f3aad803aca424f380e0f6
Joachim Bauch <bauch@struktur.de>
2018-01-04 11:17:30 +0100

    Only expose public API through library.

--- src/heif.h
+++ src/heif.h
@@ -27,6 +27,22 @@ extern "C" {
 
 #include <stdint.h>
 
+#if defined(_MSC_VER) && !defined(LIBHEIF_STATIC_BUILD)
+  #ifdef LIBHEIF_EXPORTS
+  #define LIBHEIF_API __declspec(dllexport)
+  #else
+  #define LIBHEIF_API __declspec(dllimport)
+  #endif
+#elif HAVE_VISIBILITY
+  #ifdef LIBHEIF_EXPORTS
+  #define LIBHEIF_API __attribute__((__visibility__("default")))
+  #else
+  #define LIBHEIF_API
+  #endif
+#else
+  #define LIBHEIF_API
+#endif
+
 struct heif_context;  // TODO  heif_context == HeifFile, which is not so nice
 struct heif_image_handle;
 struct heif_image;
@@ -39,49 +55,63 @@ struct heif_error
 };
 
 
+LIBHEIF_API
 struct heif_context* heif_context_alloc();
 
+LIBHEIF_API
 void heif_context_free(struct heif_context*);
 
+LIBHEIF_API
 struct heif_error heif_context_read_from_file(struct heif_context*, const char* filename);
 
+LIBHEIF_API
 struct heif_error heif_context_read_from_memory(struct heif_context*,
                                                 const uint8_t* mem, uint64_t size);
 
 // TODO
+LIBHEIF_API
 struct heif_error heif_context_read_from_file_descriptor(struct heif_context*, int fd);
 
+LIBHEIF_API
 struct heif_error heif_context_get_primary_image_handle(struct heif_context* h,
                                                         struct heif_image_handle**);
 
 
 // --- heif_image_handle
 
+LIBHEIF_API
 int heif_context_get_number_of_images(struct heif_context* h);
 
+LIBHEIF_API
 struct heif_error heif_context_get_image_handle(struct heif_context* h,
                                                 int image_index,
                                                 struct heif_image_handle**);
 
+LIBHEIF_API
 int heif_image_handle_is_primary_image(const struct heif_context* h,
                                        const struct heif_image_handle* handle);
 
+LIBHEIF_API
 int heif_image_handle_get_number_of_thumbnails(const struct heif_context* h,
                                                const struct heif_image_handle* handle);
 
+LIBHEIF_API
 void heif_image_handle_get_thumbnail(const struct heif_context* h,
                                      const struct heif_image_handle* handle,
                                      int thumbnail_idx,
                                      struct heif_image_handle** out_thumbnail_handle);
 
+LIBHEIF_API
 void heif_image_handle_get_resolution(const struct heif_context* h,
                                       const struct heif_image_handle* handle,
                                       int* width, int* height);
 
+LIBHEIF_API
 int  heif_image_handle_get_exif_data_size(const struct heif_context* h,
                                           const struct heif_image_handle* handle);
 
 // out_data must point to a memory area of size heif_image_handle_get_exif_data_size().
+LIBHEIF_API
 void heif_image_handle_get_exif_data(const struct heif_context* h,
                                      const struct heif_image_handle* handle,
                                      uint8_t* out_data);
@@ -127,34 +157,44 @@ enum heif_channel {
 
 // If colorspace or chroma is set up heif_colorspace_undefined or heif_chroma_undefined,
 // respectively, the original colorspace is taken.
+LIBHEIF_API
 struct heif_error heif_decode_image(struct heif_context* ctx,
                                     const struct heif_image_handle* in_handle,
                                     struct heif_image** out_img,
                                     enum heif_colorspace colorspace,
                                     enum heif_chroma chroma);
 
+LIBHEIF_API
 enum heif_compression_format heif_image_get_compression_format(struct heif_image*);
 
+LIBHEIF_API
 int heif_image_get_width(const struct heif_image*,enum heif_channel channel);
 
+LIBHEIF_API
 int heif_image_get_height(const struct heif_image*,enum heif_channel channel);
 
+LIBHEIF_API
 enum heif_chroma heif_image_get_chroma_format(const struct heif_image*);
 
+LIBHEIF_API
 int heif_image_get_bits_per_pixel(const struct heif_image*,enum heif_channel channel);
 
 /* The |out_stride| is returned as "bytes per line".
    When out_stride is NULL, no value will be written. */
+LIBHEIF_API
 const uint8_t* heif_image_get_plane_readonly(const struct heif_image*,
                                              enum heif_channel channel,
                                              int* out_stride);
 
+LIBHEIF_API
 uint8_t* heif_image_get_plane(struct heif_image*,
                               enum heif_channel channel,
                               int* out_stride);
 
+LIBHEIF_API
 void heif_image_release(const struct heif_image*);
 
+LIBHEIF_API
 void heif_image_handle_release(const struct heif_image_handle*);
 
 
@@ -179,10 +219,12 @@ void heif_image_free_data_chunk(heif_image* img, int chunk_index);
 // Create a new image of the specified resolution and colorspace.
 // Note: no memory for the actual image data is reserved yet. You have to use
 // heif_image_add_plane() to add the image planes required by your colorspace/chroma.
+LIBHEIF_API
 struct heif_image* heif_image_create(int width, int height,
                                      enum heif_colorspace colorspace,
                                      enum heif_chroma chroma);
 
+LIBHEIF_API
 void heif_image_add_plane(struct heif_image* image,
                           enum heif_channel channel, int width, int height, int bit_depth);
 
@@ -213,6 +255,7 @@ struct heif_decoder_plugin
 
 
 
+LIBHEIF_API
 void heif_register_decoder(struct heif_context* heif, uint32_t type, const struct heif_decoder_plugin*);
 
 // TODO void heif_register_encoder(heif_file* heif, uint32_t type, const heif_encoder_plugin*);
