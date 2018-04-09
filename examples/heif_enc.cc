771d412bd634851e1820fe70007cb5ddd7f4e4b7
Dirk Farin <farin@struktur.de>
2018-04-09 18:10:30 +0200

    heif-enc: add loading of PNG images

--- examples/heif_enc.cc
+++ examples/heif_enc.cc
@@ -28,6 +28,7 @@
 #include <fstream>
 #include <iostream>
 #include <memory>
+#include <algorithm>
 
 #include "heif.h"
 
@@ -36,6 +37,14 @@ extern "C" {
 #include <jpeglib.h>
 }
 #endif
+
+#if HAVE_LIBPNG
+extern "C" {
+#include <png.h>
+}
+#endif
+
+#include <assert.h>
 
 
 static struct option long_options[] = {
@@ -209,7 +218,6 @@ std::shared_ptr<heif_image> loadJPEG(const char* filename)
 
   return std::shared_ptr<heif_image>(image,
                                     [] (heif_image* img) { heif_image_release(img); });
-
 }
 #else
 std::shared_ptr<heif_image> loadJPEG(const char* filename)
@@ -220,6 +228,256 @@ std::shared_ptr<heif_image> loadJPEG(const char* filename)
   return nullptr;
 }
 #endif
+
+
+
+
+#if HAVE_LIBPNG
+static void
+user_read_fn(png_structp png_ptr, png_bytep data, png_size_t length)
+{
+  FILE* fh = (FILE*)png_get_io_ptr(png_ptr);
+  int n = fread((char*)data,length,1,fh);
+  (void)n;
+} // user_read_data
+
+
+std::shared_ptr<heif_image> loadPNG(const char* filename)
+{
+  FILE* fh = fopen(filename,"rb");
+  if (!fh) {
+    std::cerr << "Can't open " << filename << "\n";
+    exit(1);
+  }
+
+
+  // ### Code copied from LibVideoGfx and slightly modified to use HeifPixelImage
+
+  struct heif_image* image = nullptr;
+
+  png_structp png_ptr;
+  png_infop info_ptr;
+  png_uint_32 width, height;
+  int bit_depth, color_type, interlace_type;
+
+  /* Create and initialize the png_struct with the desired error handler
+   * functions.  If you want to use the default stderr and longjump method,
+   * you can supply NULL for the last three parameters.  We also supply the
+   * the compiler header file version, so that we know if the application
+   * was compiled with a compatible version of the library.  REQUIRED
+   */
+  png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+  assert(png_ptr != NULL);
+
+  /* Allocate/initialize the memory for image information.  REQUIRED. */
+  info_ptr = png_create_info_struct(png_ptr);
+  if (info_ptr == NULL) {
+    png_destroy_read_struct(&png_ptr, (png_infopp)NULL, (png_infopp)NULL);
+    assert(false); // , "could not create info_ptr");
+  } // if
+
+    /* Set error handling if you are using the setjmp/longjmp method (this is
+     * the normal method of doing things with libpng).  REQUIRED unless you
+     * set up your own error handlers in the png_create_read_struct() earlier.
+     */
+  if (setjmp(png_jmpbuf(png_ptr))) {
+    /* Free all of the memory associated with the png_ptr and info_ptr */
+    png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+    /* If we get here, we had a problem reading the file */
+    assert(false); // , "fatal error in png library");
+  } // if
+
+  /* If you are using replacement read functions, instead of calling
+   * png_init_io() here you would call: */
+  png_set_read_fn(png_ptr, (void *)fh, user_read_fn);
+  /* where user_io_ptr is a structure you want available to the callbacks */
+
+  /* The call to png_read_info() gives us all of the information from the
+   * PNG file before the first IDAT (image data chunk).  REQUIRED
+   */
+  png_read_info(png_ptr, info_ptr);
+
+  png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
+               &interlace_type, NULL, NULL);
+
+  assert(bit_depth < 16); // , "cannot handle 16 bit images");
+
+  /**** Set up the data transformations you want.  Note that these are all
+   **** optional.  Only call them if you want/need them.  Many of the
+   **** transformations only work on specific types of images, and many
+   **** are mutually exclusive.
+   ****/
+
+  // \TODO
+  //      /* Strip alpha bytes from the input data without combining with the
+  //       * background (not recommended).
+  //       */
+  //      png_set_strip_alpha(png_ptr);
+
+  /* Extract multiple pixels with bit depths of 1, 2, and 4 from a single
+   * byte into separate bytes (useful for paletted and grayscale images).
+   */
+  png_set_packing(png_ptr);
+
+
+  /* Expand paletted colors into true RGB triplets */
+  if (color_type == PNG_COLOR_TYPE_PALETTE)
+    png_set_expand(png_ptr);
+
+  /* Expand grayscale images to the full 8 bits from 1, 2, or 4 bits/pixel */
+  if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+    png_set_expand(png_ptr);
+
+  /* Set the background color to draw transparent and alpha images over.
+   * It is possible to set the red, green, and blue components directly
+   * for paletted images instead of supplying a palette index.  Note that
+   * even if the PNG file supplies a background, you are not required to
+   * use it - you should use the (solid) application background if it has one.
+   */
+
+#if 0
+  // \TODO 0 is index in color lookup table - correct? used already?
+  png_color_16 my_background = {0, 255, 255, 255, 255};
+  png_color_16 *image_background;
+
+  if (png_get_bKGD(png_ptr, info_ptr, &image_background))
+    png_set_background(png_ptr, image_background, PNG_BACKGROUND_GAMMA_FILE, 1, 1.0);
+  else
+    png_set_background(png_ptr, &my_background, PNG_BACKGROUND_GAMMA_SCREEN, 0, 1.0);
+#endif
+
+
+  /* Optional call to gamma correct and add the background to the palette
+   * and update info structure.  REQUIRED if you are expecting libpng to
+   * update the palette for you (ie you selected such a transform above).
+   */
+  png_read_update_info(png_ptr, info_ptr);
+
+  /* Allocate the memory to hold the image using the fields of info_ptr. */
+
+  /* The easiest way to read the image: */
+  uint8_t** row_pointers = new png_bytep[height];
+  assert(row_pointers != NULL);
+
+  for (uint32_t y = 0; y < height; y++) {
+    row_pointers[y] = (png_bytep)malloc(png_get_rowbytes(png_ptr, info_ptr));
+    assert(row_pointers[y] != NULL);
+  } // for
+
+  /* Now it's time to read the image.  One of these methods is REQUIRED */
+  png_read_image(png_ptr, row_pointers);
+
+  /* read rest of file, and get additional chunks in info_ptr - REQUIRED */
+  png_read_end(png_ptr, info_ptr);
+
+  /* clean up after the read, and free any memory allocated - REQUIRED */
+  png_destroy_read_struct(&png_ptr, &info_ptr, (png_infopp)NULL);
+
+
+  // OK, now we should have the png image in some way in
+  // row_pointers, have fun with it
+
+  int band;
+  switch (color_type) {
+  case PNG_COLOR_TYPE_GRAY:
+  case PNG_COLOR_TYPE_GRAY_ALPHA:
+    band = 1;
+    break;
+  case PNG_COLOR_TYPE_PALETTE:
+  case PNG_COLOR_TYPE_RGB:
+  case PNG_COLOR_TYPE_RGB_ALPHA:
+    band = 3;
+    break;
+  default:
+    assert(false); // , "unknown color type in png image.");
+  } // switch
+
+
+
+
+  struct heif_error err;
+
+  bool has_alpha = (color_type & PNG_COLOR_MASK_ALPHA);
+
+  if (band==1) {
+    err = heif_image_create(width, height,
+                            heif_colorspace_YCbCr,
+                            heif_chroma_monochrome,
+                            &image);
+    (void)err;
+
+    heif_image_add_plane(image, heif_channel_Y, width, height, 8);
+
+    int y_stride;
+    int a_stride;
+    uint8_t* py  = heif_image_get_plane(image, heif_channel_Y, &y_stride);
+    uint8_t* pa  = nullptr;
+
+    if (has_alpha) {
+      heif_image_add_plane(image, heif_channel_Alpha, width, height, 8);
+
+      pa = heif_image_get_plane(image, heif_channel_Alpha, &a_stride);
+    }
+
+
+    for (uint32_t y = 0; y < height; y++) {
+      uint8_t* p = row_pointers[y];
+
+      if (has_alpha)
+        {
+          for (uint32_t x = 0; x < width; x++) {
+            py[y*y_stride + x] = *p++;
+            pa[y*a_stride + x] = *p++;
+          }
+        }
+      else
+        {
+          memcpy(&py[y*y_stride],p, width);
+        }
+    }
+  }
+  else {
+    err = heif_image_create(width, height,
+                            heif_colorspace_RGB,
+                            has_alpha ? heif_chroma_interleaved_RGBA : heif_chroma_interleaved_RGB,
+                            &image);
+    (void)err;
+
+    heif_image_add_plane(image, heif_channel_interleaved, width, height,
+                         has_alpha ? 32 : 24);
+
+    int stride;
+    uint8_t* p = heif_image_get_plane(image, heif_channel_interleaved, &stride);
+
+    for (uint32_t y = 0; y < height; y++) {
+      if (has_alpha) {
+        memcpy(p + y*stride, row_pointers[y], width*4);
+      }
+      else {
+        memcpy(p + y*stride, row_pointers[y], width*3);
+      }
+    }
+  }
+
+  for (uint32_t y = 0; y < height; y++) {
+    free(row_pointers[y]);
+  } // for
+
+  delete[] row_pointers;
+
+  return std::shared_ptr<heif_image>(image,
+                                    [] (heif_image* img) { heif_image_release(img); });
+}
+#else
+std::shared_ptr<heif_image> loadPNG(const char* filename)
+{
+  std::cerr << "Cannot load PNG because libpng support was not compiled.\n";
+  exit(1);
+
+  return nullptr;
+}
+#endif
+
 
 
 int main(int argc, char** argv)
@@ -299,7 +557,27 @@ int main(int argc, char** argv)
   }
 
 
-  std::shared_ptr<heif_image> image = loadJPEG(input_filename.c_str());
+  // get file type from file name
+
+  std::string suffix;
+  auto suffix_pos = input_filename.find_last_of('.');
+  if (suffix_pos != std::string::npos) {
+    suffix = input_filename.substr(suffix_pos+1);
+    std::transform(suffix.begin(), suffix.end(), suffix.begin(), ::tolower);
+  }
+
+  enum { PNG, JPEG } filetype = JPEG;
+  if (suffix == "png") {
+    filetype = PNG;
+  }
+
+  std::shared_ptr<heif_image> image;
+  if (filetype==PNG) {
+    image = loadPNG(input_filename.c_str());
+  }
+  else {
+    image = loadJPEG(input_filename.c_str());
+  }
 
 
 
