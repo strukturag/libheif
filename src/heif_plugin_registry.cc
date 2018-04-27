741871006be405dbbce4640bea708ea2279e4247
Alex Gaynor <alex.gaynor@gmail.com>
2018-04-27 15:49:52 -0400

    Mark operator() on comparator function as const (#13)
    Mark operator() on comparator function as const

--- src/heif_plugin_registry.cc
+++ src/heif_plugin_registry.cc
@@ -39,7 +39,7 @@ std::set<const struct heif_decoder_plugin*> heif::s_decoder_plugins;
 struct encoder_descriptor_priority_order
 {
   bool operator() (const std::unique_ptr<struct heif_encoder_descriptor>& a,
-                   const std::unique_ptr<struct heif_encoder_descriptor>& b) {
+                   const std::unique_ptr<struct heif_encoder_descriptor>& b) const {
     return a->plugin->priority > b->plugin->priority;  // highest priority first
   }
 };
