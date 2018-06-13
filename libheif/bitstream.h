2ab7cc6b7bf4f24b394a825d1e3c2de05a968a36
Dirk Farin <farin@struktur.de>
2018-06-13 12:11:29 +0200

    define StreamReader interface and provide istream implementation

--- libheif/bitstream.h
+++ libheif/bitstream.h
@@ -43,6 +43,85 @@
 
 namespace heif {
 
+  class StreamReader
+  {
+  public:
+    virtual ~StreamReader() { }
+
+    virtual int64_t get_position() const = 0;
+    virtual int64_t get_length() const = 0; // note: files may grow while reading
+
+    enum grow_status {
+      size_reached,   // requested size has been reached
+      timeout,        // size has not been reached yet, but it may still grow further
+      size_beyond_eof // size has not been reached and never will. The file has grown to its full size
+    };
+
+    virtual grow_status wait_for_file_size(int64_t target_size, int32_t timeout_ms) {
+      return target_size <= get_length() ? size_reached : size_beyond_eof;
+    }
+
+    // returns 'false' when we read out of the available file size
+    virtual bool    read(void* data, size_t size) = 0;
+
+    virtual bool    can_seek_backwards() const { return false; }
+    virtual bool    seek_abs(int64_t position) = 0;
+    virtual bool    seek_cur(int64_t position_offset) {
+      return seek_abs(get_position() + position_offset);
+    }
+  };
+
+
+  class StreamReader_istream : public StreamReader
+  {
+  public:
+    StreamReader_istream(std::istream* istr)
+      : m_istr(istr)
+    {
+      istr->seekg(0, std::ios_base::end);
+      m_length = istr->tellg();
+      istr->seekg(0, std::ios_base::beg);
+    }
+
+    int64_t get_position() const { return m_istr->tellg(); }
+    int64_t get_length() const { return m_length; }
+
+    // returns 'false' when we read out of the available file size
+    virtual bool    read(void* data, size_t size) {
+      int64_t end_pos = get_position() + size;
+      if (end_pos > m_length) {
+        return false;
+      }
+
+      m_istr->read((char*)data, size);
+      return true;
+    }
+
+    virtual bool    can_seek_backwards() const { return true; }
+
+    virtual bool    seek_abs(int64_t position) {
+      if (position>=m_length)
+        return false;
+
+      m_istr->seekg(position, std::ios_base::beg);
+      return true;
+    }
+
+    virtual bool    seek_cur(int64_t position_offset) {
+      int64_t target_pos = (get_position() + position_offset);
+      if (target_pos < 0 || target_pos >= m_length)
+        return false;
+
+      m_istr->seekg(position_offset, std::ios_base::cur);
+      return true;
+    }
+
+  private:
+    std::istream* m_istr;
+    int64_t m_length;
+  };
+
+
   class BitstreamRange
   {
   public:
