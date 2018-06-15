8e20f1ca5b9f8b0214811b17d932797314c2d804
Dirk Farin <farin@struktur.de>
2018-06-15 10:48:45 +0200

    add test compiling heif.h with plain C compiler

---
+++ examples/test_c_api.c
@@ -0,0 +1,28 @@
+/*
+ * libheif testing application
+ * Copyright (c) 2017 struktur AG, Dirk Farin <farin@struktur.de>
+ *
+ * This file is part of libheif. It performs test calculations for CI checks.
+ *
+ * heif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * heif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with heif.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+// Very simple test whether the header file compiler with a plain C compiler
+
+#include "libheif/heif.h"
+
+int main()
+{
+  return 0;
+}
