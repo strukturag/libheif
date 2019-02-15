cce27e3be27084a2e93407cc69478c0f8dd462df
Dirk Farin <farin@struktur.de>
2019-02-15 13:18:16 +0100

    cleanup AV1 decoder plugin

--- libheif/heif_decoder_aom.cc
+++ libheif/heif_decoder_aom.cc
@@ -37,7 +37,6 @@ struct aom_decoder
 {
   aom_codec_ctx_t codec;
   aom_codec_iface_t* iface;
-  //const AvxInterface *decoder = NULL;
 };
 
 static const char kSuccess[] = "Success";
@@ -65,13 +64,11 @@ static const char* aom_plugin_name()
 
 static void aom_init_plugin()
 {
-  //de265_init();
 }
 
 
 static void aom_deinit_plugin()
 {
-  //de265_free();
 }
 
 
@@ -86,73 +83,18 @@ static int aom_does_support_format(enum heif_compression_format format)
 }
 
 
-/*
-struct heif_error convert_libde265_image_to_heif_image(struct libde265_decoder* decoder,
-                                                       const struct de265_image* de265img, struct heif_image** image)
-{
-  struct heif_image* out_img;
-  struct heif_error err = heif_image_create(
-    de265_get_image_width(de265img, 0),
-    de265_get_image_height(de265img, 0),
-    heif_colorspace_YCbCr, // TODO
-    (heif_chroma)de265_get_chroma_format(de265img),
-    &out_img);
-  if (err.code != heif_error_Ok) {
-    return err;
-  }
-
-  // --- transfer data from de265_image to HeifPixelImage
-
-  heif_channel channel2plane[3] = {
-    heif_channel_Y,
-    heif_channel_Cb,
-    heif_channel_Cr
-  };
-
-
-  for (int c=0;c<3;c++) {
-    int bpp = de265_get_bits_per_pixel(de265img, c);
-
-    int stride;
-    const uint8_t* data = de265_get_image_plane(de265img, c, &stride);
-
-    int w = de265_get_image_width(de265img, c);
-    int h = de265_get_image_height(de265img, c);
-
-    err = heif_image_add_plane(out_img, channel2plane[c], w,h, bpp);
-    if (err.code != heif_error_Ok) {
-      heif_image_release(out_img);
-      return err;
-    }
-
-    int dst_stride;
-    uint8_t* dst_mem = heif_image_get_plane(out_img, channel2plane[c], &dst_stride);
-
-    int bytes_per_pixel = (bpp+7)/8;
-
-    for (int y=0;y<h;y++) {
-      memcpy(dst_mem + y*dst_stride, data + y*stride, w * bytes_per_pixel);
-    }
-  }
-
-  *image = out_img;
-  return err;
-}
-*/
-
 struct heif_error aom_new_decoder(void** dec)
 {
   struct aom_decoder* decoder = new aom_decoder();
-
-  //decoder->decoder = get_aom_decoder_by_fourcc(AV1_FOURCC);
 
   decoder->iface = aom_codec_av1_dx();
 
   aom_codec_err_t aomerr = aom_codec_dec_init(&decoder->codec, decoder->iface, NULL, 0);
   if (aomerr) {
-    //die_codec(&codec, "Failed to initialize decoder.");
-    printf("failed to initialize decoder %d\n", aomerr);
-    exit(10);
+    *dec = NULL;
+
+    struct heif_error err = { heif_error_Decoder_plugin_error, heif_suberror_Unspecified, kSuccess };
+    return err;
   }
 
   *dec = decoder;
@@ -161,14 +103,15 @@ struct heif_error aom_new_decoder(void** dec)
   return err;
 }
 
+
 void aom_free_decoder(void* decoder_raw)
 {
-  //struct libde265_decoder* decoder = (struct libde265_decoder*)decoder_raw;
+  struct aom_decoder* decoder = (aom_decoder*)decoder_raw;
 
   //de265_error err = de265_free_decoder(decoder->ctx);
   //(void)err;
 
-  //delete decoder;
+  delete decoder;
 }
 
 
@@ -176,55 +119,13 @@ struct heif_error aom_push_data(void* decoder_raw, const void* frame_data, size_
 {
   struct aom_decoder* decoder = (struct aom_decoder*)decoder_raw;
 
-  printf("in: %d bytes\n", (int)frame_size);
-
-  for (int i=0;i<64;i++) {
-    printf("%02x ", ((uint8_t*)frame_data)[i]);
-    if ((i%16)==15) printf("\n");
-  }
-
   aom_codec_err_t aomerr;
   aomerr = aom_codec_decode(&decoder->codec, (const uint8_t*)frame_data, frame_size, NULL);
   if (aomerr) {
-    printf("ierr: %d\n", aomerr);
-
-    struct heif_error err = { heif_error_Ok, heif_suberror_Unspecified, kSuccess };
+    struct heif_error err = { heif_error_Invalid_input, heif_suberror_Unspecified, kSuccess };
     return err;
   }
 
-  printf("i0\n");
-
-  /*
-  const uint8_t* cdata = (const uint8_t*)data;
-
-  size_t ptr=0;
-  while (ptr < size) {
-    if (ptr+4 > size) {
-      struct heif_error err = { heif_error_Decoder_plugin_error,
-                                heif_suberror_End_of_data,
-                                kEmptyString };
-      return err;
-    }
-
-    // TODO: the size of the NAL unit length variable is defined in the hvcC header.
-    // We should not assume that it is always 4 bytes.
-    uint32_t nal_size = (cdata[ptr]<<24) | (cdata[ptr+1]<<16) | (cdata[ptr+2]<<8) | (cdata[ptr+3]);
-    ptr+=4;
-
-    if (ptr+nal_size > size) {
-      //sstr << "NAL size (" << size32 << ") exceeds available data in file ("
-      //<< data_bytes_left_to_read << ")";
-
-      struct heif_error err = { heif_error_Decoder_plugin_error,
-                                heif_suberror_End_of_data,
-                                kEmptyString };
-      return err;
-    }
-
-    de265_push_NAL(decoder->ctx, cdata+ptr, nal_size, 0, nullptr);
-    ptr += nal_size;
-  }
-  */
 
   struct heif_error err = { heif_error_Ok, heif_suberror_Unspecified, kSuccess };
   return err;
@@ -249,6 +150,13 @@ struct heif_error aom_decode_image(void* decoder_raw, struct heif_image** out_im
 
 
 
+  if (img->fmt != AOM_IMG_FMT_I420) {
+    struct heif_error err = { heif_error_Decoder_plugin_error,
+                              heif_suberror_Unsupported_image_type,
+                              kEmptyString };
+    return err;
+  }
+
   struct heif_image* heif_img;
   struct heif_error err = heif_image_create(img->w, img->h,
                                             heif_colorspace_YCbCr,
@@ -260,7 +168,7 @@ struct heif_error aom_decode_image(void* decoder_raw, struct heif_image** out_im
 
 
 
-  // --- transfer data from de265_image to HeifPixelImage
+  // --- transfer data from aom_image_t to HeifPixelImage
 
   heif_channel channel2plane[3] = {
     heif_channel_Y,
@@ -268,8 +176,6 @@ struct heif_error aom_decode_image(void* decoder_raw, struct heif_image** out_im
     heif_channel_Cr
   };
 
-
-  printf("wh: %d %d\n",img->w, img->h);
 
   for (int c=0;c<3;c++) {
     int bpp = img->bit_depth;
@@ -296,12 +202,9 @@ struct heif_error aom_decode_image(void* decoder_raw, struct heif_image** out_im
     }
   }
 
-  printf("i1\n");
-
   *out_img = heif_img;
   return err;
 }
-
 
 
 static const struct heif_decoder_plugin decoder_aom
@@ -316,6 +219,7 @@ static const struct heif_decoder_plugin decoder_aom
   aom_push_data,
   aom_decode_image
 };
+
 
 const struct heif_decoder_plugin* get_decoder_plugin_aom() {
   return &decoder_aom;
