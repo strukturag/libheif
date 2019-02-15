09727bf2ff4691f63bb09d022a846a78e8f3f222
Dirk Farin <farin@struktur.de>
2019-02-15 14:28:47 +0100

    check that tile images have same chroma as combined image

--- libheif/error.cc
+++ libheif/error.cc
@@ -97,6 +97,7 @@ const char* heif::Error::get_error_string(heif_suberror_code err)
   case heif_suberror_Auxiliary_image_type_unspecified: return "Type of auxiliary image unspecified";
   case heif_suberror_No_or_invalid_primary_item: return "No or invalid primary item";
   case heif_suberror_Unknown_color_profile_type: return "Unknown color profile type";
+  case heif_suberror_Wrong_tile_image_chroma_format: return "Wrong tile image chroma format";
 
     // --- Memory_allocation_error ---
 
--- libheif/heif_emscripten.h
+++ libheif/heif_emscripten.h
@@ -222,6 +222,7 @@ EMSCRIPTEN_BINDINGS(libheif) {
     .value("heif_suberror_No_infe_box",heif_suberror_No_infe_box)
     .value("heif_suberror_Security_limit_exceeded",heif_suberror_Security_limit_exceeded)
     .value("heif_suberror_Unknown_color_profile_type",heif_suberror_Unknown_color_profile_type)
+    .value("heif_suberror_Wrong_tile_image_chroma_format",heif_suberror_Wrong_tile_image_chroma_format)
     .value("heif_suberror_Nonexisting_item_referenced",heif_suberror_Nonexisting_item_referenced)
     .value("heif_suberror_Null_pointer_argument",heif_suberror_Null_pointer_argument)
     .value("heif_suberror_Nonexisting_image_channel_referenced",heif_suberror_Nonexisting_image_channel_referenced)
