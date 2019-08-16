5af68f3e345394ded2824c2250834b334887bd3c
Dirk Farin <farin@struktur.de>
2019-08-16 11:26:53 +0200

    remove unnecessary check

--- libheif/heif_context.cc
+++ libheif/heif_context.cc
@@ -309,7 +309,7 @@ void ImageOverlay::get_background_color(uint16_t col[4]) const
 
 void ImageOverlay::get_offset(size_t image_index, int32_t* x, int32_t* y) const
 {
-  assert(image_index>=0 && image_index<m_offsets.size());
+  assert(image_index<m_offsets.size());
   assert(x && y);
 
   *x = m_offsets[image_index].x;
