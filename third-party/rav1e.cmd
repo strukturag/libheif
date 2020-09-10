1367471149d4ecbd5a7b472d78cd9bb56d6abc27
Aditya Patadia <adityapatadia@users.noreply.github.com>
2020-09-10 11:57:59 +0530

    update rav1e to 0.3.4

--- third-party/rav1e.cmd
+++ third-party/rav1e.cmd
@@ -10,7 +10,7 @@
 : # Also, the error that "The target windows-msvc is not supported yet" can safely be ignored provided that rav1e/target/release
 : # contains rav1e.h and rav1e.lib.
 
-git clone -b v0.3.3 --depth 1 https://github.com/xiph/rav1e.git
+git clone -b v0.3.4 --depth 1 https://github.com/xiph/rav1e.git
 
 cd rav1e
 cargo install cbindgen
