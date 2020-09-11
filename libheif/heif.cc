bb1f69264f15b98ced9c4500c43dc08163bb3bb9
Jamaika1 <lukaszcz18@wp.pl>
2020-09-11 07:32:34 +0200

    Update heif.cc

--- libheif/heif.cc
+++ libheif/heif.cc
@@ -43,12 +43,13 @@
 #include <utility>
 #include <vector>
 #include <string.h>
+#if defined(HAVE_UNISTD_H)
+#include <unistd.h>
+#ednif
 
-#if defined(__MINGW32__)  || defined(__MINGW64__) || defined(_MSC_VER) || !defined(HAVE_UNISTD_H) 
+#if defined(__MINGW32__)  || defined(__MINGW64__) || defined(_MSC_VER)
 // for _write
 #include <io.h>
-#else
-#include <unistd.h>
 #endif
 
 using namespace heif;
@@ -323,7 +324,7 @@ void heif_context_debug_dump_boxes_to_file(struct heif_context* ctx, int fd)
 
   std::string dump = ctx->context->debug_dump_boxes();
   // TODO(fancycode): Should we return an error if writing fails?
-#if defined(__MINGW32__)  || defined(__MINGW64__) || defined(_MSC_VER) || !defined(HAVE_UNISTD_H)
+#if defined(__MINGW32__)  || defined(__MINGW64__) || defined(_MSC_VER)
   auto written = _write(fd, dump.c_str(), dump.size());
 #else
   auto written = write(fd, dump.c_str(), dump.size());
