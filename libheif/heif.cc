fc6315a6eb01e68cc23fe64197d1495db39543d1
Jamaika1 <lukaszcz18@wp.pl>
2020-09-11 08:38:40 +0200

    Update heif.cc

--- libheif/heif.cc
+++ libheif/heif.cc
@@ -323,7 +323,7 @@ void heif_context_debug_dump_boxes_to_file(struct heif_context* ctx, int fd)
 
   std::string dump = ctx->context->debug_dump_boxes();
   // TODO(fancycode): Should we return an error if writing fails?
-#if defined(_MSC_VER)
+#if (defined(__MINGW32__)  || defined(__MINGW64__) || defined(_MSC_VER)) && !defined(HAVE_UNISTD_H)
   auto written = _write(fd, dump.c_str(), dump.size());
 #else
   auto written = write(fd, dump.c_str(), dump.size());
