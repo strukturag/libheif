c0b1933ea1a105325bbe6bc3d32069a6d1dc6caf
Jamaika1 <lukaszcz18@wp.pl>
2020-12-17 21:37:46 +0100

    Correction spacing

--- examples/heif_enc.cc
+++ examples/heif_enc.cc
@@ -39,7 +39,7 @@
 #include <string>
 
 #include <libheif/heif.h>
-//#if HAVE_AOM_DECODER || HAVE_AOM_ENCODER
+//#if HAVE_AOM_ENCODER
 //#include <config/aom_version.h>
 //#endif
 
@@ -1216,25 +1216,25 @@ int main(int argc, char** argv)
     {
       if (enc_av1f == true)
       {
-        std::cerr << "\nLibrary encoder:   libavif  HDR " << heif_get_version() << " 8<>12bit c++\n";
-//#if HAVE_AOM_DECODER || HAVE_AOM_ENCODER
-        //std::cerr << "                   libaom       " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << " 8<>12bit c\n";
+        std::cerr << "\nLibrary encoder:   libavif HDR 8<>12bit c++ " << heif_get_version() << "\n";
+//#if HAVE_AOM_ENCODER
+        //std::cerr << "                   libaom      8<>12bit c  " << VERSION_MAJOR << "." << VERSION_MINOR << "." << VERSION_PATCH << "\n";
 //#endif
       }
       else
       {
-        std::cerr << "\nLibrary encoder:   libheif  HDR " << heif_get_version() << " 8<>12bit c++\n";
+        std::cerr << "\nLibrary encoder:   libheif HDR 8<>12bit c++" << heif_get_version() << "\n";
       }
 #if HAVE_LIBJPEG
 #ifdef LIBJPEG_TURBO_VERSION_NUMBER
-      std::cerr << "                   libJPEGturbo " << xstr(LIBJPEG_TURBO_VERSION) << "     8bit c\n";
+      std::cerr << "                   libJPEGturbo    8bit c  " << xstr(LIBJPEG_TURBO_VERSION) << "\n";
 #else
-      std::cerr << "                   libJPEG      " << JPEG_LIB_VERSION_MAJOR << "." << JPEG_LIB_VERSION_MINOR << "        8bit c\n";
+      std::cerr << "                   libJPEG         8bit c  " << JPEG_LIB_VERSION_MAJOR << "." << JPEG_LIB_VERSION_MINOR << "\n";
 #endif
 #endif
 #if HAVE_LIBPNG
-      std::cerr << "                   libPNG       " << PNG_LIBPNG_VER_MAJOR << "." << PNG_LIBPNG_VER_MINOR << "." << PNG_LIBPNG_VER_RELEASE << "          c\n";
-      std::cerr << "                     zlib       " << ZLIB_VER_MAJOR << "." << ZLIB_VER_MINOR << "." << ZLIB_VER_REVISION << "." << ZLIB_VER_SUBREVISION <<"        c\n\n";
+      std::cerr << "                   libPNG               c  " << PNG_LIBPNG_VER_MAJOR << "." << PNG_LIBPNG_VER_MINOR << "." << PNG_LIBPNG_VER_RELEASE << "\n";
+      std::cerr << "                     zlib               c  " << ZLIB_VER_MAJOR << "." << ZLIB_VER_MINOR << "." << ZLIB_VER_REVISION << "." << ZLIB_VER_SUBREVISION <<"\n\n";
 #endif
     }
 
