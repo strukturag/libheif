3c638046755cd145094ae21610fd550368cd903a
Jamaika1 <lukaszcz18@wp.pl>
2021-02-26 08:50:54 +0100

    Change speed integer maximum
    AOM 2.1.0-rc1

--- libheif/heif_encoder_aom.cc
+++ libheif/heif_encoder_aom.cc
@@ -126,7 +126,7 @@ static void aom_init_parameters()
   p->has_default = true;
   p->integer.have_minimum_maximum = true;
   p->integer.minimum = 0;
-  p->integer.maximum = 8;
+  p->integer.maximum = 9;
   p->integer.valid_values = NULL;
   p->integer.num_valid_values = 0;
   d[i++] = p++;
