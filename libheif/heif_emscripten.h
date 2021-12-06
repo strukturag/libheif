d13161376c4aa9e9ddb343fa5799aaef35675827
Aider <ideeer4@gmail.com>
2021-12-06 21:21:06 +0300

    Stride -> width

--- libheif/heif_emscripten.h
+++ libheif/heif_emscripten.h
@@ -34,7 +34,7 @@ static void strided_copy(void *dest, const void *src, int width, int height,
   {
     const uint8_t *_src = static_cast<const uint8_t *>(src);
     uint8_t *_dest = static_cast<uint8_t *>(dest);
-    for (int y = 0; y < height; y++, _dest += stride, _src += stride)
+    for (int y = 0; y < height; y++, _dest += width, _src += stride)
     {
       memcpy(_dest, _src, stride);
     }
