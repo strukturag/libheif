b406a079c10bfed22c6d3997d94b4a34434b1d44
Aider <ideeer4@gmail.com>
2021-12-09 00:22:58 +0300

    Call typo

--- libheif/heif_emscripten.h
+++ libheif/heif_emscripten.h
@@ -189,7 +189,7 @@ static emscripten::val heif_js_decode_image(struct heif_image_handle *handle,
     // Should never reach here.
     break;
   }
-  result.set("data", std::move(data.data));
+  result.set("data", std::move(data.data()));
   heif_image_release(image);
   return result;
 }
