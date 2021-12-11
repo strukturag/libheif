6ff5c6b944954e536b72646870baa1d1b4278446
Aider <ideeer4@gmail.com>
2021-12-11 16:27:16 +0300

    Raw pointers in return

--- libheif/heif_emscripten.h
+++ libheif/heif_emscripten.h
@@ -136,23 +136,23 @@ static emscripten::val heif_js_decode_image(struct heif_image_handle *handle,
     const uint8_t *plane_u = heif_image_get_plane_readonly(image, heif_channel_Cb, nullptr);
     const uint8_t *plane_v = heif_image_get_plane_readonly(image, heif_channel_Cr, nullptr);
 
-    result.set("y", std::move(std::string(plane_y, plane_y + width * height - 1)));
-    result.set("u", std::move(std::string(plane_u, plane_u + half_width * half_height - 1)));
-    result.set("v", std::move(std::string(plane_v, plane_v + half_width * half_height - 1)));
+    result.set("y", std::move(plane_y));
+    result.set("u", std::move(plane_u));
+    result.set("v", std::move(plane_v));
   }
   break;
   case heif_colorspace_RGB:
   {
     assert(heif_image_get_chroma_format(image) == heif_chroma_interleaved_24bit);
     const uint8_t *plane_rgb = heif_image_get_plane_readonly(image, heif_channel_interleaved, nullptr);
-    result.set("rgb", std::move(std::string(plane_rgb, plane_rgb + 3 * width * height - 1)));
+    result.set("rgb", std::move(plane_rgb));
   }
   break;
   case heif_colorspace_monochrome:
   {
     assert(heif_image_get_chroma_format(image) == heif_chroma_monochrome);
     const uint8_t *plane_grey = heif_image_get_plane_readonly(image, heif_channel_Y, nullptr);
-    result.set("y", std::move(std::string(plane_grey, plane_grey + width * height - 1)));
+    result.set("y", std::move(plane_grey));
   }
   break;
   default:
