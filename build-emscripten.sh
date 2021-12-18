37499d34cb171e547789302d002c31e5e02b75b3
Aider <ideeer4@gmail.com>
2021-12-18 19:04:19 +0300

    Options

--- build-emscripten.sh
+++ build-emscripten.sh
@@ -42,7 +42,9 @@ emcc libheif/.libs/libheif.so \
         --bind \
         -s WASM=0 \
         -s ALLOW_MEMORY_GROWTH=1 \
+        -s MALLOC=emmalloc \
         -s MODULARIZE=1 \
+        -s EXPORT_ES6=1 \
         -s EXPORT_NAME="libheif" \
         --memory-init-file 0 \
         -O3 \
