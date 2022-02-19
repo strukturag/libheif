fa3ede77bfb744c941ba39458192a80c59680034
Dirk Farin <dirk.farin@gmail.com>
2022-02-19 20:21:22 +0100

    Revert "Merge pull request #565 from xyproto/master"
    This reverts commit a91dc23009822c436fbf2d1ea7c9eb2556402afe, reversing
    changes made to 8c5465e3a80db00bc5d033c7313a0a6314462f1b.

--- go/heif/heif.go
+++ go/heif/heif.go
@@ -587,6 +587,8 @@ func (h *ImageHandle) GetThumbnail(thumbnail_id int) (*ImageHandle, error) {
 	return &handle, convertHeifError(err)
 }
 
+// TODO: EXIF metadata
+
 // --- Image
 
 type DecodingOptions struct {
