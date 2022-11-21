03fb8f092248bd4f76fad5399f4e3a9d3db4dba2
Dirk Farin <dirk.farin@gmail.com>
2022-11-21 12:37:53 +0100

    fix variable type

--- libheif/box.cc
+++ libheif/box.cc
@@ -2304,7 +2304,7 @@ Error Box_imir::parse(BitstreamRange& range)
 {
   //parse_full_box_header(range);
 
-  uint16_t axis = range.read8();
+  uint8_t axis = range.read8();
   if (axis & 1) {
     m_axis = MirrorDirection::Horizontal;
   }
