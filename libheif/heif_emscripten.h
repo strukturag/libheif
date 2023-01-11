03db9fb196501ff6690af2238d0355d98f768d5c
Joachim Bauch <bauch@struktur.de>
2023-01-11 08:41:36 +0100

    JS: Fix copying of strided image data.
    Issue reported by Eugene Lim.

--- libheif/heif_emscripten.h
+++ libheif/heif_emscripten.h
@@ -32,7 +32,7 @@ static void strided_copy(void* dest, const void* src, int width, int height,
     const uint8_t* _src = static_cast<const uint8_t*>(src);
     uint8_t* _dest = static_cast<uint8_t*>(dest);
     for (int y = 0; y < height; y++, _dest += width, _src += stride) {
-      memcpy(_dest, _src, stride);
+      memcpy(_dest, _src, width);
     }
   }
 }
