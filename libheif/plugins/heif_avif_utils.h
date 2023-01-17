135f4ba0e2f13bedff4c81fc62bb49ae4bc61181
Dirk Farin <dirk.farin@gmail.com>
2023-01-17 18:46:53 +0100

    aom encoder plugin: replace all function calls to use public API only (#745)

---
+++ libheif/plugins/heif_avif_utils.h
@@ -0,0 +1,30 @@
+/*
+ * HEIF codec.
+ * Copyright (c) 2023 Dirk Farin <dirk.farin@gmail.com>
+ *
+ * This file is part of libheif.
+ *
+ * libheif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libheif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libheif.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBHEIF_HEIF_AVIF_UTILS_H
+#define LIBHEIF_HEIF_AVIF_UTILS_H
+
+#include <cinttypes>
+#include <libheif/heif.h>
+
+uint8_t compute_avif_profile(int bits_per_pixel, heif_chroma chroma);
+
+
+#endif //LIBHEIF_HEIF_AVIF_UTILS_H
