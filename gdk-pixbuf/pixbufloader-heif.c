5c4405c5e32fa8403453158fdbdcdb062644f27c
Miloš Komarčević <4973094+kmilos@users.noreply.github.com>
2023-02-15 15:21:17 +0100

    Style changes

--- gdk-pixbuf/pixbufloader-heif.c
+++ gdk-pixbuf/pixbufloader-heif.c
@@ -137,16 +137,14 @@ static gboolean stop_load(gpointer context, GError** error)
                                     img);
 
   size_t profile_size = heif_image_handle_get_raw_color_profile_size(hdl);
-  if(profile_size)
-  {
+  if(profile_size) {
     guchar *profile_data = (guchar *)g_malloc0(profile_size);
-    if(profile_data)
-    {
+    if(profile_data) {
       heif_image_handle_get_raw_color_profile(hdl, profile_data);
       gchar *profile_base64 = g_base64_encode(profile_data, profile_size);
       gdk_pixbuf_set_option(pixbuf, "icc-profile", profile_base64);
-      g_free(profile_data);
       g_free(profile_base64);
+      g_free(profile_data);
     }
   }
   
