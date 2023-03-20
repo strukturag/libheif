strukturag/libheif

commit 135f4ba0e2f13bedff4c81fc62bb49ae4bc61181

aom encoder plugin: replace all function calls to use public API only (#745)

libheif/plugins/heif_encoder_aom.cc
@@ -20,8 +20,7 @@
 
 #include "libheif/heif.h"
 #include "libheif/heif_plugin.h"
-#include "libheif/heif_avif.h"
-#include "libheif/heif_api_structs.h"
+#include "heif_avif_utils.h"
 
 #if defined(HAVE_CONFIG_H)
 #include "config.h"
@@ -32,6 +31,7 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <memory>
 #include "heif_encoder_aom.h"
 
 #include <aom/aom_encoder.h>
@@ -858,8 +858,8 @@ struct heif_error aom_encode_image(void* encoder_raw, const struct heif_image* i
     return err;
   }
 
-  heif::Box_av1C::configuration inout_config;
-  heif::fill_av1C_configuration(&inout_config, image->image);
+  int seq_profile = compute_avif_profile(heif_image_get_bits_per_pixel(image, heif_channel_Y),
+                                     heif_image_get_chroma_format(image));
 
   cfg.g_w = source_width;
   cfg.g_h = source_height;
@@ -880,7 +880,7 @@ struct heif_error aom_encode_image(void* encoder_raw, const struct heif_image* i
   // Tell libaom that all frames will be key frames.
   cfg.kf_max_dist = 0;
 
-  cfg.g_profile = inout_config.seq_profile;
+  cfg.g_profile = seq_profile;
   cfg.g_bit_depth = (aom_bit_depth_t) bpp_y;
   cfg.g_input_bit_depth = bpp_y;
 
@@ -938,18 +938,25 @@ struct heif_error aom_encode_image(void* encoder_raw, const struct heif_image* i
   // TODO: set AV1E_SET_TILE_ROWS and AV1E_SET_TILE_COLUMNS.
 
 
-  auto nclx = image->image->get_color_profile_nclx();
+  struct heif_color_profile_nclx* nclx = nullptr;
+  err = heif_image_get_nclx_color_profile(image, &nclx);
+  if (err.code != heif_error_Ok) {
+    nclx = nullptr;
+  }
+
+  // make sure NCLX profile is deleted at end of function
+  auto nclx_deleter = std::unique_ptr<heif_color_profile_nclx, void (*)(heif_color_profile_nclx*)>(nclx, heif_nclx_color_profile_free);
 
   // In aom, color_range defaults to limited range (0). Set it to full range (1).
-  aom_codec_control(&codec, AV1E_SET_COLOR_RANGE, nclx ? nclx->get_full_range_flag() : 1);
+  aom_codec_control(&codec, AV1E_SET_COLOR_RANGE, nclx ? nclx->full_range_flag : 1);
   aom_codec_control(&codec, AV1E_SET_CHROMA_SAMPLE_POSITION, chroma_sample_position);
 
   if (nclx &&
       (input_class == heif_image_input_class_normal ||
        input_class == heif_image_input_class_thumbnail)) {
-    aom_codec_control(&codec, AV1E_SET_COLOR_PRIMARIES, nclx->get_colour_primaries());
-    aom_codec_control(&codec, AV1E_SET_MATRIX_COEFFICIENTS, nclx->get_matrix_coefficients());
-    aom_codec_control(&codec, AV1E_SET_TRANSFER_CHARACTERISTICS, nclx->get_transfer_characteristics());
+    aom_codec_control(&codec, AV1E_SET_COLOR_PRIMARIES, nclx->color_primaries);
+    aom_codec_control(&codec, AV1E_SET_MATRIX_COEFFICIENTS, nclx->matrix_coefficients);
+    aom_codec_control(&codec, AV1E_SET_TRANSFER_CHARACTERISTICS, nclx->transfer_characteristics);
   }
 
   aom_codec_control(&codec, AOME_SET_TUNING, encoder->tune);

libheif/CMakeLists.txt
@@ -39,6 +39,8 @@ add_library(heif
     logging.h
     metadata_compression.cc
     metadata_compression.h
+    plugins/heif_avif_utils.cc
+    plugins/heif_avif_utils.h
     ${libheif_headers}
     )
 

libheif/heif_avif.cc
@@ -21,6 +21,7 @@
 #include "heif_image.h"
 #include "heif_avif.h"
 #include "bitstream.h"
+#include "plugins/heif_avif_utils.h"
 #include <limits>
 
 using namespace heif;
@@ -33,20 +34,7 @@ Error heif::fill_av1C_configuration(Box_av1C::configuration* inout_config, const
   int bpp = image->get_bits_per_pixel(heif_channel_Y);
   heif_chroma chroma = image->get_chroma_format();
 
-  uint8_t profile;
-
-  if (bpp <= 10 &&
-      (chroma == heif_chroma_420 ||
-       chroma == heif_chroma_monochrome)) {
-    profile = 0;
-  }
-  else if (bpp <= 10 &&
-           chroma == heif_chroma_444) {
-    profile = 1;
-  }
-  else {
-    profile = 2;
-  }
+  uint8_t profile = compute_avif_profile(bpp, chroma);
 
   int width = image->get_width(heif_channel_Y);
   int height = image->get_height(heif_channel_Y);

libheif/plugins/CMakeLists.txt
@@ -58,7 +58,7 @@ set(AOM_DECODER_sources heif_decoder_aom.cc heif_decoder_aom.h)
 set(AOM_DECODER_extra_plugin_sources)
 plugin_compilation(aomdec AOM AOM_DECODER AOM_DECODER)
 
-set(AOM_ENCODER_sources heif_encoder_aom.cc heif_encoder_aom.h)
+set(AOM_ENCODER_sources heif_encoder_aom.cc heif_encoder_aom.h heif_avif_utils.cc heif_avif_utils.h)
 set(AOM_ENCODER_extra_plugin_sources ../heif_avif.cc ../error.cc)
 plugin_compilation(aomenc AOM AOM_ENCODER AOM_ENCODER)
 

libheif/plugins/heif_avif_utils.cc
@@ -1,0 +1,38 @@
+/*
+ * HEIF codec.
+ * Copyright (c) 2023 Dirk Farin <dirk.farin@gmail.com>
+ *
+ * This file is part of libheif.
+ *
+ * libheif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libheif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libheif.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "heif_avif_utils.h"
+
+
+uint8_t compute_avif_profile(int bits_per_pixel, heif_chroma chroma)
+{
+  if (bits_per_pixel <= 10 &&
+      (chroma == heif_chroma_420 ||
+       chroma == heif_chroma_monochrome)) {
+    return 0;
+  }
+  else if (bits_per_pixel <= 10 &&
+           chroma == heif_chroma_444) {
+    return 1;
+  }
+  else {
+    return 2;
+  }
+}

libheif/plugins/heif_avif_utils.h
@@ -1,0 +1,30 @@
+/*
+ * HEIF codec.
+ * Copyright (c) 2023 Dirk Farin <dirk.farin@gmail.com>
+ *
+ * This file is part of libheif.
+ *
+ * libheif is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * libheif is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libheif.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBHEIF_HEIF_AVIF_UTILS_H
+#define LIBHEIF_HEIF_AVIF_UTILS_H
+
+#include <cinttypes>
+#include <libheif/heif.h>
+
+uint8_t compute_avif_profile(int bits_per_pixel, heif_chroma chroma);
+
+
+#endif //LIBHEIF_HEIF_AVIF_UTILS_H

commit 45253a11d2a9d1bd13c587ab4ae3fc47472e4bff

heif_encoder_aom.cc: Get image bit depth correctly
Call heif_image_get_bits_per_pixel_range(), not
heif_image_get_bits_per_pixel(), to get the bit depth of the image. The
bug was introduced in commit 135f4ba0e2f13bedff4c81fc62bb49ae4bc61181.

Fix https://github.com/strukturag/libheif/issues/788.

libheif/plugins/heif_encoder_aom.cc
@@ -858,7 +858,7 @@ struct heif_error aom_encode_image(void* encoder_raw, const struct heif_image* i
     return err;
   }
 
-  int seq_profile = compute_avif_profile(heif_image_get_bits_per_pixel(image, heif_channel_Y),
+  int seq_profile = compute_avif_profile(heif_image_get_bits_per_pixel_range(image, heif_channel_Y),
                                      heif_image_get_chroma_format(image));
 
   cfg.g_w = source_width;