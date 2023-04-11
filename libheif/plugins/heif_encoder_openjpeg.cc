d551db533b093f2ac0c2fa6aceef069dcc738aaf
dsookhoo <dsookhoo@traxintl.com>
2023-04-11 15:37:38 -0600

    Code cleaning

--- libheif/plugins/heif_encoder_openjpeg.cc
+++ libheif/plugins/heif_encoder_openjpeg.cc
@@ -10,17 +10,12 @@
 #include <string>
 using namespace std;
 
+#define WRITE_JP2 0
 
 static const int OPJ_PLUGIN_PRIORITY = 80;
 static struct heif_error error_Ok = {heif_error_Ok, heif_suberror_Unspecified, "Success"};
 
 struct encoder_struct_opj {
-  // x265_encoder* encoder = nullptr;
-
-  // x265_nal* nals = nullptr;
-  // uint32_t num_nals = 0;
-  // uint32_t nal_output_counter = 0;
-  // int bit_depth = 0;
 
   heif_chroma chroma;
 
@@ -49,34 +44,33 @@ struct encoder_struct_opj {
   // int logLevel = X265_LOG_NONE;
 };
 
+
 const char* opj_plugin_name() {
-  // Human-readable name of the plugin
-    return "OpenJPEG JPEG2000 Encoder";
+    return "OpenJPEG JPEG2000 Encoder"; // Human-readable name of the plugin
 }
 
 void opj_init_plugin() {
   // Global plugin initialization (may be NULL)
-
 }
 
 void opj_cleanup_plugin() {
-    // Global plugin cleanup (may be NULL).
-    // Free data that was allocated in init_plugin()
+  // Global plugin cleanup (may be NULL).
+  // Free data that was allocated in init_plugin()
 }
 
 struct heif_error opj_new_encoder(void** encoder_out) {
 
-    struct encoder_struct_opj* encoder = new encoder_struct_opj();
-    encoder->chroma = heif_chroma_interleaved_RGB; //default chroma
+  struct encoder_struct_opj* encoder = new encoder_struct_opj();
+  encoder->chroma = heif_chroma_interleaved_RGB; //default chroma
 
-    *encoder_out = encoder;
-    return error_Ok;
+  *encoder_out = encoder;
+  return error_Ok;
 }
 
 void opj_free_encoder(void* encoder_raw) {
-    // Free the decoder context (heif_image can still be used after destruction)
-    struct encoder_struct_opj* encoder = (struct encoder_struct_opj*) encoder_raw;
-    delete encoder;
+  // Free the decoder context (heif_image can still be used after destruction)
+  struct encoder_struct_opj* encoder = (struct encoder_struct_opj*) encoder_raw;
+  delete encoder;
 }
 
 struct heif_error opj_set_parameter_quality(void* encoder, int quality) {
@@ -132,27 +126,12 @@ struct heif_error opj_get_parameter_string(void* encoder, const char* name, char
 }
 
 void opj_query_input_colorspace(enum heif_colorspace* inout_colorspace, enum heif_chroma* inout_chroma) {
-  //TODO
   // Replace the input colorspace/chroma with the one that is supported by the encoder and that
   // comes as close to the input colorspace/chroma as possible.
 }
 
 static OPJ_SIZE_T global_variable = 0;
-static OPJ_UINT64 opj_get_data_length_from_buffer(void * p_user_data, uint32_t size)
-{
-  printf("LINE: %d  FILE: %s  size: %x\n", __LINE__, __FILE__, size);
-#  define OPJ_FSEEK(stream,offset,whence) fseek(stream,offset,whence)
-#  define OPJ_FTELL(stream) ftell(stream)
 
-    FILE* p_file = (FILE*)p_user_data;
-    OPJ_OFF_T file_length = 0;
-
-    OPJ_FSEEK(p_file, 0, SEEK_END);
-    file_length = (OPJ_OFF_T)OPJ_FTELL(p_file);
-    OPJ_FSEEK(p_file, 0, SEEK_SET);
-
-    return (OPJ_UINT64)file_length;
-}
 static OPJ_SIZE_T opj_write_from_buffer(void * source, OPJ_SIZE_T nb_bytes, void * dest)
 {
     printf("%s()  - nb_bytes: %ld = 0x%lx\n", __FUNCTION__, nb_bytes, nb_bytes);
@@ -160,13 +139,13 @@ static OPJ_SIZE_T opj_write_from_buffer(void * source, OPJ_SIZE_T nb_bytes, void
     global_variable += nb_bytes;
     return nb_bytes;
 }
+
 static void opj_close_from_buffer(void* p_user_data)
 {
     FILE* p_file = (FILE*)p_user_data;
     fclose(p_file);
 }
 
-#define WRITE_JP2 0
 static opj_image_t *to_opj_image(const unsigned char *buf, 
                                  int width, int height, 
                                  int nr_comp, int sub_dx, 
@@ -237,90 +216,76 @@ static opj_image_t *to_opj_image(const unsigned char *buf,
 } 
 
 
-static vector<uint8_t> generate_codestream(const uint8_t* data, uint32_t width, uint32_t  height, uint32_t  numcomps) {
+static heif_error generate_codestream(const uint8_t* data, vector<uint8_t>& codestream, int width, int height, int numcomps) {
 
-
-  //Purpose - Generate a .j2k compressed file named "write_idf" from the input buffer. 
 	opj_cparameters_t parameters;
-	opj_image_t *image;
-  int sub_dx, sub_dy;
-
-
 	opj_set_default_encoder_parameters(&parameters);
+	int sub_dx = parameters.subsampling_dx;
+	int sub_dy = parameters.subsampling_dy;
+  #if 0
+  //Add comment
+	if (parameters.cp_comment == NULL) {
+    char buf[80];
+    #ifdef _WIN32
+    sprintf_s(buf, 80, "Created by OpenJPEG version %s", opj_version());
+    #else
+    snprintf(buf, 80, "Created by OpenJPEG version %s", opj_version());
+    #endif
+    parameters.cp_comment = strdup(buf);
+  }
+  #endif
 
-	if(parameters.cp_comment == NULL)
-   {
-	char buf[80];
-#ifdef _WIN32
-	sprintf_s(buf, 80, "Created by OpenJPEG version %s", opj_version());
-#else
-	snprintf(buf, 80, "Created by OpenJPEG version %s", opj_version());
-#endif
-	parameters.cp_comment = strdup(buf);
-   }
-
-    if(parameters.tcp_numlayers == 0)
-   {
-    parameters.tcp_rates[0] = 0;   /* MOD antonin : losslessbug */
+  if (parameters.tcp_numlayers == 0) {
+    parameters.tcp_rates[0] = 0;
     parameters.tcp_numlayers++;
     parameters.cp_disto_alloc = 1;
-   }
-	sub_dx = parameters.subsampling_dx;
-	sub_dy = parameters.subsampling_dy;
+  }
 
-//--------------------------------------------------------
-	image = to_opj_image(data, (int)width, (int)height,
-		(int)numcomps, sub_dx, sub_dy);
-  opj_image_comp_t r = image->comps[0];
 
-//--------------------------------------------------------
+  //Create opj image
+	opj_image_t *image = to_opj_image(data, width, height, numcomps, sub_dx, sub_dy);
 
-	opj_stream_t *stream;
 	opj_codec_t* codec;
 
-	stream = NULL;	
 	parameters.tcp_mct = image->numcomps == 3 ? 1 : 0;
 
-	OPJ_CODEC_FORMAT codec_format = OPJ_CODEC_J2K;
-	// OPJ_CODEC_FORMAT codec_format = OPJ_CODEC_JP2;
+	OPJ_CODEC_FORMAT codec_format = OPJ_CODEC_J2K; // OPJ_CODEC_JP2;
 	codec = opj_create_compress(codec_format);
 
 	opj_setup_encoder(codec, &parameters, image);
 
-	// stream = opj_stream_create_default_file_stream(write_idf, WRITE_JP2);
   size_t size = width * height * numcomps;
   uint8_t* out_data = (uint8_t*) malloc(size);
-  {
-    opj_stream_t* l_stream = 00;
+  opj_stream_t* l_stream = 00;
 
-    l_stream = opj_stream_create(OPJ_J2K_STREAM_CHUNK_SIZE, WRITE_JP2);
+  l_stream = opj_stream_create(OPJ_J2K_STREAM_CHUNK_SIZE, WRITE_JP2);
 
-    opj_stream_set_user_data(l_stream, out_data, opj_close_from_buffer);
-    // opj_stream_set_user_data_length(l_stream, 0);
-    // opj_stream_set_read_function(l_stream, opj_read_from_buffer);
-    opj_stream_set_write_function(l_stream, (opj_stream_write_fn) opj_write_from_buffer);
-    // opj_stream_set_skip_function(l_stream, opj_skip_from_buffer);
-    // opj_stream_set_seek_function(l_stream, opj_seek_from_buffer);
+  opj_stream_set_user_data(l_stream, out_data, opj_close_from_buffer);
+  opj_stream_set_write_function(l_stream, (opj_stream_write_fn) opj_write_from_buffer);
+  // opj_stream_set_user_data_length(l_stream, 0);
 
-    // typedef struct opj_codestream_info 
-    // typedef struct opj_codestream_index
-    //    - codestream_size
-
-    stream = l_stream;
-  }
-
-  vector<uint8_t> codestream;
-	if(stream == NULL) {
-    return codestream; //TODO - Handle errors properly
+	opj_stream_t *stream = l_stream;
+  heif_error error;
+	if (stream == NULL) {
+    error = { heif_error_Encoding_error,
+              heif_suberror_Unspecified,
+              "Failed to create opj_stream_t"};
+    return error;
   }
 
 
-	if( !opj_start_compress(codec, image, stream)) {
-    return codestream; //TODO - Handle errors properly
+	if (!opj_start_compress(codec, image, stream)) {
+    error = { heif_error_Encoding_error,
+              heif_suberror_Unspecified,
+              "Failed opj_start_compress()"};
+    return error;
   }
 
-	if( !opj_encode(codec, stream)) {
-    return codestream; //TODO - Handle errors properly
+	if (!opj_encode(codec, stream)) {
+    error = { heif_error_Encoding_error,
+              heif_suberror_Unspecified,
+              "Failed opj_encode()"};
+    return error;
   }
 
 	opj_end_compress(codec, stream);
@@ -329,18 +294,17 @@ static vector<uint8_t> generate_codestream(const uint8_t* data, uint32_t width, 
     codestream.push_back(out_data[i]);
   }
 
-  return codestream;
-
-
+  return error_Ok;
 }
 
 struct heif_error opj_encode_image(void* encoder_raw, const struct heif_image* image, enum heif_image_input_class image_class) {
   
   struct encoder_struct_opj* encoder = (struct encoder_struct_opj*) encoder_raw;
-  heif_chroma chroma = heif_image_get_chroma_format(image);
-  heif_colorspace colorspace = heif_image_get_colorspace(image);
   struct heif_error err;
 
+
+  heif_chroma chroma = heif_image_get_chroma_format(image);
+  heif_colorspace colorspace = heif_image_get_colorspace(image);
   uint32_t numcomps;
   heif_channel channel;
   if (chroma == heif_chroma_interleaved_RGB) {
@@ -366,24 +330,14 @@ struct heif_error opj_encode_image(void* encoder_raw, const struct heif_image* i
 
 
 
-  //GET PIXEL DATA
   int stride = 0;
   const uint8_t* src_data = heif_image_get_plane_readonly(image, channel, &stride);
-
-  //GET CODESTREAM
   unsigned int width = heif_image_get_primary_width(image);
   unsigned int height = heif_image_get_primary_height(image);
-  // vector<uint8_t> codestream = generate_codestream(src_data, width, height, numcomps);
   encoder->compressedData.clear(); //Fixes issue when encoding multiple images and old data persists.
-  encoder->compressedData = generate_codestream(src_data, width, height, numcomps);
+  err = generate_codestream(src_data, encoder->compressedData, width, height, numcomps);
 
-  // for (size_t i = 0; i < codestream.size(); i++) {
-  //   uint8_t x = codestream[i];
-  //   encoder->compressedData.push_back(x);
-  // }
-  // // encoder->compressedData.
-
-  return error_Ok;
+  return err;
 }
 
 struct heif_error opj_get_compressed_data(void* encoder_raw, uint8_t** data, int* size, enum heif_encoded_data_type* type) {
