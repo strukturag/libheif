f5b59329c1b44a6d2a3144b63da508456b839ae8
Dirk Farin <dirk.farin@gmail.com>
2023-04-29 00:58:59 +0200

    CI: run build-from-tgz test with cmake

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -237,26 +237,21 @@ if [ ! -z "$EMSCRIPTEN_VERSION" ]; then
 fi
 
 if [ ! -z "$TARBALL" ]; then
-    CONFIGURE_ARGS=
-    if [ ! -z "$GO" ]; then
-        CONFIGURE_ARGS="$CONFIGURE_ARGS --prefix=$BUILD_ROOT/dist --disable-gdk-pixbuf"
-    else
-        CONFIGURE_ARGS="$CONFIGURE_ARGS --disable-go"
-    fi
-    if [ "$WITH_RAV1E" = "1" ]; then
-        CONFIGURE_ARGS="$CONFIGURE_ARGS --enable-local-rav1e"
-    fi
-
-    VERSION=$(grep AC_INIT configure.ac | sed -r 's/^[^0-9]*([0-9]+\.[0-9]+\.[0-9]+).*/\1/g')
+    VERSION=$(grep project CMakeLists.txt | sed -r 's/^[^0-9]*([0-9]+\.[0-9]+\.[0-9]+).*/\1/g')
     echo "Creating tarball for version $VERSION ..."
-    make dist
+    mkdir build
+    pushd build
+    cmake .. --preset=release
+    make package_source
+    mv libheif-$VERSION-Source.tar.gz ..
+    popd
 
     echo "Building from tarball ..."
-    tar xf libheif-$VERSION.tar*
-    pushd libheif-$VERSION
-    mkdir -p ./third-party/
-    ln -s $BUILD_ROOT/third-party/rav1e ./third-party/
-    ./configure $CONFIGURE_ARGS
+    tar xf libheif-$VERSION-Source.tar*
+    pushd libheif-$VERSION-Source
+    mkdir build
+    pushd build
+    cmake .. --preset=release
     make -j $(nproc)
     popd
 fi
