ac692f7223587bf59fb3c65787c402d8b8ea6d9d
Dirk Farin <dirk.farin@gmail.com>
2023-05-01 11:42:04 +0200

    add 'average' chroma sampling to heif_enc help

--- examples/heif_enc.cc
+++ examples/heif_enc.cc
@@ -185,7 +185,7 @@ void show_help(const char* argv0)
             << "  --premultiplied-alpha     input image has premultiplied alpha\n"
             << "  --enable-metadata-compression   enable XMP metadata compression (experimental)\n"
 #ifdef HAVE_LIBSHARPYUV
-            << "  -C,--chroma-downsampling ALGO   force chroma downsampling algorithm (nn = nearest-neighbor / sharp-yuv)\n"
+            << "  -C,--chroma-downsampling ALGO   force chroma downsampling algorithm (nn = nearest-neighbor / average / sharp-yuv)\n"
             << "                                  (sharp-yuv makes edges look sharper when using YUV420 with bilinear chroma upsampling)\n"
 #endif
             << "  --benchmark               measure encoding time, PSNR, and output file size\n"
