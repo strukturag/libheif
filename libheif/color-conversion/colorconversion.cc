strukturag/libheif

commit 8f9ac3d6db58d91ebb36f68244cdc534253b889b

only allocate ColorConversionOperations once while the library is loaded

libheif/color-conversion/colorconversion.cc
@@ -163,7 +163,7 @@ struct Node
 {
   Node() = default;
 
-  Node(int prev, const std::shared_ptr<ColorConversionOperation>& _op, const ColorStateWithCost& state)
+  Node(int prev, const ColorConversionOperation* _op, const ColorStateWithCost& state)
   {
     prev_processed_idx = prev;
     op = _op;
@@ -171,9 +171,60 @@ struct Node
   }
 
   int prev_processed_idx = -1;
-  std::shared_ptr<ColorConversionOperation> op;
+  const ColorConversionOperation* op;
   ColorStateWithCost color_state;
 };
+
+
+std::vector<ColorConversionOperation*> ColorConversionPipeline::m_operation_pool;
+
+
+void ColorConversionPipeline::init_ops()
+{
+  if (!m_operation_pool.empty()) {
+    return;
+  }
+
+  std::vector<ColorConversionOperation*>& ops = m_operation_pool;
+  ops.push_back(new Op_RGB_to_RGB24_32());
+  ops.push_back(new Op_RGB24_32_to_RGB());
+  ops.push_back(new Op_YCbCr_to_RGB<uint16_t>());
+  ops.push_back(new Op_YCbCr_to_RGB<uint8_t>());
+  ops.push_back(new Op_YCbCr420_to_RGB24());
+  ops.push_back(new Op_YCbCr420_to_RGB32());
+  ops.push_back(new Op_YCbCr420_to_RRGGBBaa());
+  ops.push_back(new Op_RGB_HDR_to_RRGGBBaa_BE());
+  ops.push_back(new Op_RGB_to_RRGGBBaa_BE());
+  ops.push_back(new Op_mono_to_YCbCr420());
+  ops.push_back(new Op_mono_to_RGB24_32());
+  ops.push_back(new Op_RRGGBBaa_swap_endianness());
+  ops.push_back(new Op_RRGGBBaa_BE_to_RGB_HDR());
+  ops.push_back(new Op_RGB24_32_to_YCbCr());
+  ops.push_back(new Op_RGB_to_YCbCr<uint8_t,true>());
+  ops.push_back(new Op_RGB_to_YCbCr<uint16_t,true>());
+  ops.push_back(new Op_RGB_to_YCbCr<uint8_t,false>());
+  ops.push_back(new Op_RGB_to_YCbCr<uint16_t,false>());
+  ops.push_back(new Op_RRGGBBxx_HDR_to_YCbCr420());
+  ops.push_back(new Op_RGB24_32_to_YCbCr444_GBR());
+  ops.push_back(new Op_drop_alpha_plane());
+  ops.push_back(new Op_to_hdr_planes());
+  ops.push_back(new Op_to_sdr_planes());
+  ops.push_back(new Op_YCbCr420_bilinear_to_YCbCr444<uint8_t>());
+  ops.push_back(new Op_YCbCr420_bilinear_to_YCbCr444<uint16_t>());
+  ops.push_back(new Op_YCbCr444_to_YCbCr420_average<uint8_t>());
+  ops.push_back(new Op_YCbCr444_to_YCbCr420_average<uint16_t>());
+  ops.push_back(new Op_Any_RGB_to_YCbCr_420_Sharp());
+}
+
+
+void ColorConversionPipeline::release_ops()
+{
+  for (auto& op : m_operation_pool) {
+    delete op;
+  }
+
+  m_operation_pool.clear();
+}
 
 
 bool ColorConversionPipeline::construct_pipeline(const ColorState& input_state,
@@ -197,36 +248,9 @@ bool ColorConversionPipeline::construct_pipeline(const ColorState& input_state,
   print_state(std::cerr, target_state);
 #endif
 
-  std::vector<std::shared_ptr<ColorConversionOperation>> ops;
-  ops.push_back(std::make_shared<Op_RGB_to_RGB24_32>());
-  ops.push_back(std::make_shared<Op_RGB24_32_to_RGB>());
-  ops.push_back(std::make_shared<Op_YCbCr_to_RGB<uint16_t>>());
-  ops.push_back(std::make_shared<Op_YCbCr_to_RGB<uint8_t>>());
-  ops.push_back(std::make_shared<Op_YCbCr420_to_RGB24>());
-  ops.push_back(std::make_shared<Op_YCbCr420_to_RGB32>());
-  ops.push_back(std::make_shared<Op_YCbCr420_to_RRGGBBaa>());
-  ops.push_back(std::make_shared<Op_RGB_HDR_to_RRGGBBaa_BE>());
-  ops.push_back(std::make_shared<Op_RGB_to_RRGGBBaa_BE>());
-  ops.push_back(std::make_shared<Op_mono_to_YCbCr420>());
-  ops.push_back(std::make_shared<Op_mono_to_RGB24_32>());
-  ops.push_back(std::make_shared<Op_RRGGBBaa_swap_endianness>());
-  ops.push_back(std::make_shared<Op_RRGGBBaa_BE_to_RGB_HDR>());
-  ops.push_back(std::make_shared<Op_RGB24_32_to_YCbCr>());
-  ops.push_back(std::make_shared<Op_RGB_to_YCbCr<uint8_t,true>>());
-  ops.push_back(std::make_shared<Op_RGB_to_YCbCr<uint16_t,true>>());
-  ops.push_back(std::make_shared<Op_RGB_to_YCbCr<uint8_t,false>>());
-  ops.push_back(std::make_shared<Op_RGB_to_YCbCr<uint16_t,false>>());
-  ops.push_back(std::make_shared<Op_RRGGBBxx_HDR_to_YCbCr420>());
-  ops.push_back(std::make_shared<Op_RGB24_32_to_YCbCr444_GBR>());
-  ops.push_back(std::make_shared<Op_drop_alpha_plane>());
-  ops.push_back(std::make_shared<Op_to_hdr_planes>());
-  ops.push_back(std::make_shared<Op_to_sdr_planes>());
-  ops.push_back(std::make_shared<Op_YCbCr420_bilinear_to_YCbCr444<uint8_t>>());
-  ops.push_back(std::make_shared<Op_YCbCr420_bilinear_to_YCbCr444<uint16_t>>());
-  ops.push_back(std::make_shared<Op_YCbCr444_to_YCbCr420_average<uint8_t>>());
-  ops.push_back(std::make_shared<Op_YCbCr444_to_YCbCr420_average<uint16_t>>());
+  init_ops(); // to be sure these are initialized even without heif_init()
 
-  ops.push_back(std::make_shared<Op_Any_RGB_to_YCbCr_420_Sharp>());
+  std::vector<ColorConversionOperation*>& ops = m_operation_pool;
 
   // --- Dijkstra search for the minimum-cost conversion pipeline
 

libheif/color-conversion/colorconversion.h
@@ -76,18 +76,21 @@ public:
   virtual std::vector<ColorStateWithCost>
   state_after_conversion(const ColorState& input_state,
                          const ColorState& target_state,
-                         const heif_color_conversion_options& options) = 0;
+                         const heif_color_conversion_options& options) const = 0;
 
   virtual std::shared_ptr<HeifPixelImage>
   convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                      const ColorState& target_state,
-                     const heif_color_conversion_options& options) = 0;
+                     const heif_color_conversion_options& options) const = 0;
 };
 
 
 class ColorConversionPipeline
 {
 public:
+  static void init_ops();
+  static void release_ops();
+
   bool construct_pipeline(const ColorState& input_state,
                           const ColorState& target_state,
                           const heif_color_conversion_options& options);
@@ -98,7 +101,9 @@ public:
   void debug_dump_pipeline() const;
 
 private:
-  std::vector<std::shared_ptr<ColorConversionOperation>> m_operations;
+  static std::vector<ColorConversionOperation*> m_operation_pool;
+
+  std::vector<const ColorConversionOperation*> m_operations;
   ColorState m_target_state;
   heif_color_conversion_options m_options;
 };

libheif/color-conversion/alpha.cc
@@ -24,7 +24,7 @@
 std::vector<ColorStateWithCost>
 Op_drop_alpha_plane::state_after_conversion(const ColorState& input_state,
                                             const ColorState& target_state,
-                                            const heif_color_conversion_options& options)
+                                            const heif_color_conversion_options& options) const
 {
   // only drop alpha plane if it is not needed in output
 
@@ -55,7 +55,7 @@ Op_drop_alpha_plane::state_after_conversion(const ColorState& input_state,
 std::shared_ptr<HeifPixelImage>
 Op_drop_alpha_plane::convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                                         const ColorState& target_state,
-                                        const heif_color_conversion_options& options)
+                                        const heif_color_conversion_options& options) const
 {
   int width = input->get_width();
   int height = input->get_height();

libheif/color-conversion/alpha.h
@@ -32,12 +32,12 @@ public:
   std::vector<ColorStateWithCost>
   state_after_conversion(const ColorState& input_state,
                          const ColorState& target_state,
-                         const heif_color_conversion_options& options) override;
+                         const heif_color_conversion_options& options) const override;
 
   std::shared_ptr<HeifPixelImage>
   convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                      const ColorState& target_state,
-                     const heif_color_conversion_options& options) override;
+                     const heif_color_conversion_options& options) const override;
 };
 
 #endif //LIBHEIF_COLORCONVERSION_ALPHA_H

libheif/color-conversion/hdr_sdr.cc
@@ -24,7 +24,7 @@
 std::vector<ColorStateWithCost>
 Op_to_hdr_planes::state_after_conversion(const ColorState& input_state,
                                          const ColorState& target_state,
-                                         const heif_color_conversion_options& options)
+                                         const heif_color_conversion_options& options) const
 {
   if ((input_state.chroma != heif_chroma_monochrome &&
        input_state.chroma != heif_chroma_420 &&
@@ -52,7 +52,7 @@ Op_to_hdr_planes::state_after_conversion(const ColorState& input_state,
 std::shared_ptr<HeifPixelImage>
 Op_to_hdr_planes::convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                                      const ColorState& target_state,
-                                     const heif_color_conversion_options& options)
+                                     const heif_color_conversion_options& options) const
 {
   auto outimg = std::make_shared<HeifPixelImage>();
 
@@ -105,7 +105,7 @@ Op_to_hdr_planes::convert_colorspace(const std::shared_ptr<const HeifPixelImage>
 std::vector<ColorStateWithCost>
 Op_to_sdr_planes::state_after_conversion(const ColorState& input_state,
                                          const ColorState& target_state,
-                                         const heif_color_conversion_options& options)
+                                         const heif_color_conversion_options& options) const
 {
   if ((input_state.chroma != heif_chroma_monochrome &&
        input_state.chroma != heif_chroma_420 &&
@@ -137,7 +137,7 @@ Op_to_sdr_planes::state_after_conversion(const ColorState& input_state,
 std::shared_ptr<HeifPixelImage>
 Op_to_sdr_planes::convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                                      const ColorState& target_state,
-                                     const heif_color_conversion_options& options)
+                                     const heif_color_conversion_options& options) const
 {
 
   auto outimg = std::make_shared<HeifPixelImage>();

libheif/color-conversion/hdr_sdr.h
@@ -32,12 +32,12 @@ public:
   std::vector<ColorStateWithCost>
   state_after_conversion(const ColorState& input_state,
                          const ColorState& target_state,
-                         const heif_color_conversion_options& options) override;
+                         const heif_color_conversion_options& options) const override;
 
   std::shared_ptr<HeifPixelImage>
   convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                      const ColorState& target_state,
-                     const heif_color_conversion_options& options) override;
+                     const heif_color_conversion_options& options) const override;
 };
 
 
@@ -47,12 +47,12 @@ public:
   std::vector<ColorStateWithCost>
   state_after_conversion(const ColorState& input_state,
                          const ColorState& target_state,
-                         const heif_color_conversion_options& options) override;
+                         const heif_color_conversion_options& options) const override;
 
   std::shared_ptr<HeifPixelImage>
   convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                      const ColorState& target_state,
-                     const heif_color_conversion_options& options) override;
+                     const heif_color_conversion_options& options) const override;
 };
 
 

libheif/color-conversion/monochrome.cc
@@ -25,7 +25,7 @@
 std::vector<ColorStateWithCost>
 Op_mono_to_YCbCr420::state_after_conversion(const ColorState& input_state,
                                             const ColorState& target_state,
-                                            const heif_color_conversion_options& options)
+                                            const heif_color_conversion_options& options) const
 {
   if (input_state.colorspace != heif_colorspace_monochrome ||
       input_state.chroma != heif_chroma_monochrome) {
@@ -52,7 +52,7 @@ Op_mono_to_YCbCr420::state_after_conversion(const ColorState& input_state,
 std::shared_ptr<HeifPixelImage>
 Op_mono_to_YCbCr420::convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                                         const ColorState& target_state,
-                                        const heif_color_conversion_options& options)
+                                        const heif_color_conversion_options& options) const
 {
   auto outimg = std::make_shared<HeifPixelImage>();
 
@@ -158,7 +158,7 @@ Op_mono_to_YCbCr420::convert_colorspace(const std::shared_ptr<const HeifPixelIma
 std::vector<ColorStateWithCost>
 Op_mono_to_RGB24_32::state_after_conversion(const ColorState& input_state,
                                             const ColorState& target_state,
-                                            const heif_color_conversion_options& options)
+                                            const heif_color_conversion_options& options) const
 {
   // Note: no input alpha channel required. It will be filled up with 0xFF.
 
@@ -201,7 +201,7 @@ Op_mono_to_RGB24_32::state_after_conversion(const ColorState& input_state,
 std::shared_ptr<HeifPixelImage>
 Op_mono_to_RGB24_32::convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                                         const ColorState& target_state,
-                                        const heif_color_conversion_options& options)
+                                        const heif_color_conversion_options& options) const
 {
   int width = input->get_width();
   int height = input->get_height();

libheif/color-conversion/monochrome.h
@@ -32,12 +32,12 @@ public:
   std::vector<ColorStateWithCost>
   state_after_conversion(const ColorState& input_state,
                          const ColorState& target_state,
-                         const heif_color_conversion_options& options) override;
+                         const heif_color_conversion_options& options) const override;
 
   std::shared_ptr<HeifPixelImage>
   convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                      const ColorState& target_state,
-                     const heif_color_conversion_options& options) override;
+                     const heif_color_conversion_options& options) const override;
 };
 
 
@@ -47,12 +47,12 @@ public:
   std::vector<ColorStateWithCost>
   state_after_conversion(const ColorState& input_state,
                          const ColorState& target_state,
-                         const heif_color_conversion_options& options) override;
+                         const heif_color_conversion_options& options) const override;
 
   std::shared_ptr<HeifPixelImage>
   convert_colorspace(const std::shared_ptr<const HeifPixelImage>& input,
                      const ColorState& target_state,
-                     const heif_color_conversion_options& options) override;
+                     const heif_color_conversion_options& options) const override;
 };
 
 

commit 9f9e084608f4fd38fe6abac85d97a5c7316d44c9

Fix a mem leak when `heif_deinit()` was not called
By switching to `std::shared_ptr` we ensure that these smart
pointers' lifetimes persist throughout the entire execution time of
the program, eliminating the requirement of calling `heif_deinit()`.

This is especially useful in fuzzer environments where calling
`heif_deinit()` is not always feasible.

Regressed since commit 8f9ac3d6db58d91ebb36f68244cdc534253b889b.

libheif/color-conversion/colorconversion.cc
@@ -151,7 +151,7 @@ struct Node
 {
   Node() = default;
 
-  Node(int prev, const ColorConversionOperation* _op, const ColorStateWithCost& state)
+  Node(int prev, const std::shared_ptr<ColorConversionOperation>& _op, const ColorStateWithCost& state)
   {
     prev_processed_idx = prev;
     op = _op;
@@ -159,7 +159,7 @@ struct Node
   }
 
   int prev_processed_idx = -1;
-  const ColorConversionOperation* op;
+  std::shared_ptr<ColorConversionOperation> op;
   ColorStateWithCost color_state;
 };
 
@@ -170,7 +170,7 @@ std::ostream& operator<<(std::ostream& ostr, const ColorState& state) {
               << " nclx=" << (state.nclx_profile ? "yes" : "no");
 }
 
-std::vector<ColorConversionOperation*> ColorConversionPipeline::m_operation_pool;
+std::vector<std::shared_ptr<ColorConversionOperation>> ColorConversionPipeline::m_operation_pool;
 
 
 void ColorConversionPipeline::init_ops()
@@ -179,42 +179,38 @@ void ColorConversionPipeline::init_ops()
     return;
   }
 
-  std::vector<ColorConversionOperation*>& ops = m_operation_pool;
-  ops.push_back(new Op_RGB_to_RGB24_32());
-  ops.push_back(new Op_RGB24_32_to_RGB());
-  ops.push_back(new Op_YCbCr_to_RGB<uint16_t>());
-  ops.push_back(new Op_YCbCr_to_RGB<uint8_t>());
-  ops.push_back(new Op_YCbCr420_to_RGB24());
-  ops.push_back(new Op_YCbCr420_to_RGB32());
-  ops.push_back(new Op_YCbCr420_to_RRGGBBaa());
-  ops.push_back(new Op_RGB_HDR_to_RRGGBBaa_BE());
-  ops.push_back(new Op_RGB_to_RRGGBBaa_BE());
-  ops.push_back(new Op_mono_to_YCbCr420());
-  ops.push_back(new Op_mono_to_RGB24_32());
-  ops.push_back(new Op_RRGGBBaa_swap_endianness());
-  ops.push_back(new Op_RRGGBBaa_BE_to_RGB_HDR());
-  ops.push_back(new Op_RGB24_32_to_YCbCr());
-  ops.push_back(new Op_RGB_to_YCbCr<uint8_t>());
-  ops.push_back(new Op_RGB_to_YCbCr<uint16_t>());
-  ops.push_back(new Op_RRGGBBxx_HDR_to_YCbCr420());
-  ops.push_back(new Op_RGB24_32_to_YCbCr444_GBR());
-  ops.push_back(new Op_drop_alpha_plane());
-  ops.push_back(new Op_to_hdr_planes());
-  ops.push_back(new Op_to_sdr_planes());
-  ops.push_back(new Op_YCbCr420_bilinear_to_YCbCr444<uint8_t>());
-  ops.push_back(new Op_YCbCr420_bilinear_to_YCbCr444<uint16_t>());
-  ops.push_back(new Op_YCbCr444_to_YCbCr420_average<uint8_t>());
-  ops.push_back(new Op_YCbCr444_to_YCbCr420_average<uint16_t>());
-  ops.push_back(new Op_Any_RGB_to_YCbCr_420_Sharp());
+  std::vector<std::shared_ptr<ColorConversionOperation>>& ops = m_operation_pool;
+  ops.push_back(std::make_shared<Op_RGB_to_RGB24_32>());
+  ops.push_back(std::make_shared<Op_RGB24_32_to_RGB>());
+  ops.push_back(std::make_shared<Op_YCbCr_to_RGB<uint16_t>>());
+  ops.push_back(std::make_shared<Op_YCbCr_to_RGB<uint8_t>>());
+  ops.push_back(std::make_shared<Op_YCbCr420_to_RGB24>());
+  ops.push_back(std::make_shared<Op_YCbCr420_to_RGB32>());
+  ops.push_back(std::make_shared<Op_YCbCr420_to_RRGGBBaa>());
+  ops.push_back(std::make_shared<Op_RGB_HDR_to_RRGGBBaa_BE>());
+  ops.push_back(std::make_shared<Op_RGB_to_RRGGBBaa_BE>());
+  ops.push_back(std::make_shared<Op_mono_to_YCbCr420>());
+  ops.push_back(std::make_shared<Op_mono_to_RGB24_32>());
+  ops.push_back(std::make_shared<Op_RRGGBBaa_swap_endianness>());
+  ops.push_back(std::make_shared<Op_RRGGBBaa_BE_to_RGB_HDR>());
+  ops.push_back(std::make_shared<Op_RGB24_32_to_YCbCr>());
+  ops.push_back(std::make_shared<Op_RGB_to_YCbCr<uint8_t>>());
+  ops.push_back(std::make_shared<Op_RGB_to_YCbCr<uint16_t>>());
+  ops.push_back(std::make_shared<Op_RRGGBBxx_HDR_to_YCbCr420>());
+  ops.push_back(std::make_shared<Op_RGB24_32_to_YCbCr444_GBR>());
+  ops.push_back(std::make_shared<Op_drop_alpha_plane>());
+  ops.push_back(std::make_shared<Op_to_hdr_planes>());
+  ops.push_back(std::make_shared<Op_to_sdr_planes>());
+  ops.push_back(std::make_shared<Op_YCbCr420_bilinear_to_YCbCr444<uint8_t>>());
+  ops.push_back(std::make_shared<Op_YCbCr420_bilinear_to_YCbCr444<uint16_t>>());
+  ops.push_back(std::make_shared<Op_YCbCr444_to_YCbCr420_average<uint8_t>>());
+  ops.push_back(std::make_shared<Op_YCbCr444_to_YCbCr420_average<uint16_t>>());
+  ops.push_back(std::make_shared<Op_Any_RGB_to_YCbCr_420_Sharp>());
 }
 
 
 void ColorConversionPipeline::release_ops()
 {
-  for (auto& op : m_operation_pool) {
-    delete op;
-  }
-
   m_operation_pool.clear();
 }
 
@@ -238,7 +234,7 @@ bool ColorConversionPipeline::construct_pipeline(const ColorState& input_state,
 
   init_ops(); // to be sure these are initialized even without heif_init()
 
-  std::vector<ColorConversionOperation*>& ops = m_operation_pool;
+  std::vector<std::shared_ptr<ColorConversionOperation>>& ops = m_operation_pool;
 
   // --- Dijkstra search for the minimum-cost conversion pipeline
 

libheif/color-conversion/colorconversion.h
@@ -101,10 +101,10 @@ public:
   std::string debug_dump_pipeline() const;
 
 private:
-  static std::vector<ColorConversionOperation*> m_operation_pool;
+  static std::vector<std::shared_ptr<ColorConversionOperation>> m_operation_pool;
 
   struct ConversionStep {
-    const ColorConversionOperation* operation;
+    std::shared_ptr<ColorConversionOperation> operation;
     ColorState output_state;
   };
 