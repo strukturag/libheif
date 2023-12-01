32b316e23b2a3d59f00f13be33bfdb7fafef74e3
Brad Hards <bradh@frogmouth.net>
2023-12-01 17:06:49 +1100

    idat: protect against trying to output negative size

--- libheif/box.cc
+++ libheif/box.cc
@@ -2854,7 +2854,11 @@ std::string Box_idat::dump(Indent& indent) const
   std::ostringstream sstr;
   sstr << Box::dump(indent);
 
-  sstr << indent << "number of data bytes: " << get_box_size() - get_header_size() << "\n";
+  if (get_box_size() >= get_header_size()) {
+    sstr << indent << "number of data bytes: " << get_box_size() - get_header_size() << "\n";
+  } else {
+     sstr << indent << "number of data bytes is invalid\n";
+  }
 
   return sstr.str();
 }
