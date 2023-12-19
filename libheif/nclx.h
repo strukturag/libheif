68a553410f6b5f0e4f15d3990e9973584b646b6e
Kleis Auke Wolthuizen <github@kleisauke.nl>
2023-12-19 15:46:05 +0100

    Minor style change

--- libheif/nclx.h
+++ libheif/nclx.h
@@ -159,7 +159,7 @@ public:
 
   void set_from_heif_color_profile_nclx(const struct heif_color_profile_nclx* nclx);
 
-  void replace_undefined_values_with_sRGB_defaults(bool lossless=false);
+  void replace_undefined_values_with_sRGB_defaults(bool lossless = false);
 
 private:
   uint16_t m_colour_primaries = heif_color_primaries_unspecified;
