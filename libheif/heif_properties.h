3bd9b7fed0c0c4bb1e5d522ca797de494aac49bf
Dirk Farin <dirk.farin@gmail.com>
2024-06-26 19:19:24 +0200

    move camera parameters cmin/cmex to heif_experimental

--- libheif/heif_properties.h
+++ libheif/heif_properties.h
@@ -38,9 +38,7 @@ enum heif_item_property_type
   heif_item_property_type_transform_rotation = heif_fourcc('i', 'r', 'o', 't'),
   heif_item_property_type_transform_crop = heif_fourcc('c', 'l', 'a', 'p'),
   heif_item_property_type_image_size = heif_fourcc('i', 's', 'p', 'e'),
-  heif_item_property_type_uuid = heif_fourcc('u', 'u', 'i', 'd'),
-  heif_item_property_type_camera_intrinsic_matrix = heif_fourcc('c', 'm', 'i', 'n'),
-  heif_item_property_type_camera_extrinsic_matrix = heif_fourcc('c', 'm', 'e', 'x')
+  heif_item_property_type_uuid = heif_fourcc('u', 'u', 'i', 'd')
 };
 
 // Get the heif_property_id for a heif_item_id.
@@ -168,78 +166,6 @@ struct heif_error heif_item_get_property_raw_data(const struct heif_context* con
                                                   heif_item_id itemId,
                                                   heif_property_id propertyId,
                                                   uint8_t* data_out);
-
-
-LIBHEIF_API
-struct heif_error heif_item_get_property_camera_intrinsic_matrix(const struct heif_context* context,
-                                                                 heif_item_id itemId,
-                                                                 heif_property_id propertyId,
-                                                                 struct heif_property_camera_intrinsic_matrix** out_matrix);
-
-LIBHEIF_API
-void heif_property_camera_intrinsic_matrix_release(struct heif_property_camera_intrinsic_matrix* matrix);
-
-LIBHEIF_API
-struct heif_error heif_property_camera_intrinsic_matrix_get_focal_length(const struct heif_property_camera_intrinsic_matrix* matrix,
-                                                                int image_width, int image_height,
-                                                                double* out_focal_length_x,
-                                                                double* out_focal_length_y);
-
-LIBHEIF_API
-struct heif_error heif_property_camera_intrinsic_matrix_get_principal_point(const struct heif_property_camera_intrinsic_matrix* matrix,
-                                                                   int image_width, int image_height,
-                                                                   double* out_principal_point_x,
-                                                                   double* out_principal_point_y);
-
-LIBHEIF_API
-struct heif_error heif_property_camera_intrinsic_matrix_get_skew(const struct heif_property_camera_intrinsic_matrix* matrix,
-                                                        double* out_skew);
-
-LIBHEIF_API
-struct heif_property_camera_intrinsic_matrix* heif_property_camera_intrinsic_matrix_alloc();
-
-LIBHEIF_API
-void heif_property_camera_intrinsic_matrix_set_simple(struct heif_property_camera_intrinsic_matrix* matrix,
-                                             int image_width, int image_height,
-                                             double focal_length, double principal_point_x, double principal_point_y);
-
-LIBHEIF_API
-void heif_property_camera_intrinsic_matrix_set_full(struct heif_property_camera_intrinsic_matrix* matrix,
-                                           int image_width, int image_height,
-                                           double focal_length_x,
-                                           double focal_length_y,
-                                           double principal_point_x, double principal_point_y,
-                                           double skew);
-
-LIBHEIF_API
-struct heif_error heif_item_add_property_camera_intrinsic_matrix(const struct heif_context* context,
-                                                          heif_item_id itemId,
-                                                          const struct heif_property_camera_intrinsic_matrix* matrix,
-                                                          heif_property_id* out_propertyId);
-
-
-LIBHEIF_API
-struct heif_error heif_item_get_property_camera_extrinsic_matrix(const struct heif_context* context,
-                                                                 heif_item_id itemId,
-                                                                 heif_property_id propertyId,
-                                                                 struct heif_property_camera_extrinsic_matrix** out_matrix);
-
-LIBHEIF_API
-void heif_property_camera_extrinsic_matrix_release(struct heif_property_camera_extrinsic_matrix* matrix);
-
-// `out_matrix` must point to a 9-element matrix, which will be filled in row-major order.
-LIBHEIF_API
-struct heif_error heif_property_camera_extrinsic_matrix_get_rotation_matrix(const struct heif_property_camera_extrinsic_matrix* matrix,
-                                                                            double* out_matrix);
-
-// `out_vector` must point to a 3-element vector, which will be filled with the (X,Y,Z) coordinates (in micrometers).
-LIBHEIF_API
-struct heif_error heif_property_camera_extrinsic_matrix_get_position_vector(const struct heif_property_camera_extrinsic_matrix* matrix,
-                                                                            int32_t* out_vector);
-
-LIBHEIF_API
-struct heif_error heif_property_camera_extrinsic_matrix_get_world_coordinate_system_id(const struct heif_property_camera_extrinsic_matrix* matrix,
-                                                                                       uint32_t* out_wcs_id);
 
 #ifdef __cplusplus
 }
