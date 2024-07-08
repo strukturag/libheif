6826a9d7d808351f4a510c3939af9924ee31c950
Dirk Farin <dirk.farin@gmail.com>
2024-07-08 19:09:15 +0200

    AOM encoder: default number of threads should not exceed maximum (#1221)

--- libheif/plugins/encoder_aom.cc
+++ libheif/plugins/encoder_aom.cc
@@ -238,16 +238,17 @@ static void aom_init_parameters()
   p->version = 2;
   p->name = kParam_threads;
   p->type = heif_encoder_parameter_type_integer;
-  unsigned int threads = std::thread::hardware_concurrency();
-  if (threads == 0) {
-    // Could not autodetect, use previous default value.
-    threads = 4;
-  }
-  p->integer.default_value = threads;
   p->has_default = true;
   p->integer.have_minimum_maximum = true;
   p->integer.minimum = 1;
   p->integer.maximum = 64;
+  int threads = static_cast<int>(std::thread::hardware_concurrency());
+  if (threads == 0) {
+    // Could not autodetect, use previous default value.
+    threads = 4;
+  }
+  threads = std::min(threads, p->integer.maximum);
+  p->integer.default_value = threads;
   p->integer.valid_values = NULL;
   p->integer.num_valid_values = 0;
   d[i++] = p++;
