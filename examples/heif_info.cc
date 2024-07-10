9551445cd6c7e1e1347db43bb3c877aa264c48b6
Brad Hards <bradh@frogmouth.net>
2024-07-10 16:39:51 +1000

    examples: display non-visual colorspace entry in heif-info

--- examples/heif_info.cc
+++ examples/heif_info.cc
@@ -273,6 +273,9 @@ int main(int argc, char** argv)
       case heif_colorspace_monochrome:
         printf("monochrome");
         break;
+      case heif_colorspace_nonvisual:
+        printf("non-visual");
+        break;
       default:
         printf("unknown");
         break;
