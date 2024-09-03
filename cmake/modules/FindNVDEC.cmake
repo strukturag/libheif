e2768b4e736a7ee27442e6291f54318077f89c53
Brad Hards <bradh@frogmouth.net>
2024-09-03 14:04:26 +1000

    nvdec: remove mandatory requirement for CUDA

--- cmake/modules/FindNVDEC.cmake
+++ cmake/modules/FindNVDEC.cmake
@@ -4,7 +4,7 @@ find_library(NVDEC_LIBRARY
     NAMES libnvcuvid nvcuvid
 )
 
-find_package(CUDAToolkit REQUIRED)
+find_package(CUDAToolkit)
 
 set(NVDEC_PROCESS_LIBS NVDEC_LIBRARY)
 libfind_process(NVDEC)
