ea2d3c73f7a251932045f58607ebd1110d0cc367
Dirk Farin <dirk.farin@gmail.com>
2024-10-06 21:45:48 +0200

    remove heif_channel_other* from emscripten API

--- libheif/api/libheif/heif_emscripten.h
+++ libheif/api/libheif/heif_emscripten.h
@@ -456,9 +456,7 @@ EMSCRIPTEN_BINDINGS(libheif) {
     .value("heif_channel_interleaved", heif_channel_interleaved)
     .value("heif_channel_filter_array", heif_channel_filter_array)
     .value("heif_channel_depth", heif_channel_depth)
-    .value("heif_channel_disparity", heif_channel_disparity)
-    .value("heif_channel_other_first", heif_channel_other_first)
-    .value("heif_channel_other_last", heif_channel_other_last);
+    .value("heif_channel_disparity", heif_channel_disparity);
 
     emscripten::enum_<heif_filetype_result>("heif_filetype_result")
     .value("heif_filetype_no", heif_filetype_no)
