f700f715280e88a3575bc492dbe12d661bf27580
Dirk Farin <dirk.farin@gmail.com>
2024-10-22 20:34:23 +0200

    fix logic error

--- libheif/image-items/unc_image.cc
+++ libheif/image-items/unc_image.cc
@@ -468,11 +468,12 @@ void ImageItem_uncompressed::get_tile_size(uint32_t& w, uint32_t& h) const
   auto uncC = get_file()->get_property<Box_uncC>(get_id());
 
   if (!ispe || !uncC) {
-    w=h=0;
+    w = h = 0;
   }
-
-  w = ispe->get_width() / uncC->get_number_of_tile_columns();
-  h = ispe->get_height() / uncC->get_number_of_tile_rows();
+  else {
+    w = ispe->get_width() / uncC->get_number_of_tile_columns();
+    h = ispe->get_height() / uncC->get_number_of_tile_rows();
+  }
 }
 
 
