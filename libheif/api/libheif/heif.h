3dd7019ff579c038cba96353390cd41edfda927e
Dirk Farin <dirk.farin@gmail.com>
2024-11-07 19:21:12 +0100

    revert changing the constness in the API

--- libheif/api/libheif/heif.h
+++ libheif/api/libheif/heif.h
@@ -2363,7 +2363,7 @@ struct heif_encoding_options
 
   // Set this to the NCLX parameters to be used in the output image or set to NULL
   // when the same parameters as in the input image should be used.
-  const struct heif_color_profile_nclx* output_nclx_profile;
+  struct heif_color_profile_nclx* output_nclx_profile;
 
   uint8_t macOS_compatibility_workaround_no_nclx_profile;
 
--- libheif/context.cc
+++ libheif/context.cc
@@ -1162,7 +1162,7 @@ Result<std::shared_ptr<ImageItem>> HeifContext::encode_image(const std::shared_p
   heif_encoding_options options = in_options;
 
   if (const auto* nclx = output_image_item->get_forced_output_nclx()) {
-    options.output_nclx_profile = nclx;
+    options.output_nclx_profile = const_cast<heif_color_profile_nclx*>(nclx);
   }
 
   Result<std::shared_ptr<HeifPixelImage>> srcImageResult = output_image_item->convert_colorspace_for_encoding(pixel_image,
