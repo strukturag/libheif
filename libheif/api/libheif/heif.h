strukturag/libheif

commit ebf6c61260307b0514c134232f79b006a8960b49

metadata compression cleanup - support for brotli metadata decompression - rename deflate,zlib compression methods - replace WITH_DEFLATE_HEADER_COMPRESSION with WITH_HEADER_COMPRESSION

libheif/api/libheif/heif.h
@@ -2169,10 +2169,12 @@ struct heif_error heif_context_encode_thumbnail(struct heif_context*,
 
 enum heif_metadata_compression
 {
-  heif_metadata_compression_off,
-  heif_metadata_compression_auto,
-  heif_metadata_compression_deflate,
-  heif_metadata_compression_unknown
+  heif_metadata_compression_off = 0,
+  heif_metadata_compression_auto = 1,
+  heif_metadata_compression_unknown = 2, // only used when reading unknown method from input file
+  heif_metadata_compression_deflate = 3,
+  heif_metadata_compression_zlib = 4,    // do not use for header data
+  heif_metadata_compression_brotli = 5
 };
 
 // Assign 'thumbnail_image' as the thumbnail image of 'master_image'.

CMakeLists.txt
@@ -395,7 +395,7 @@ endif()
 if (LIBSHARPYUV_FOUND)
     list(APPEND REQUIRES_PRIVATE "libsharpyuv")
 endif()
-if (WITH_DEFLATE_HEADER_COMPRESSION OR WITH_UNCOMPRESSED_CODEC)
+if (WITH_HEADER_COMPRESSION OR WITH_UNCOMPRESSED_CODEC)
     list(APPEND REQUIRES_PRIVATE "zlib")
 endif()
 if (WITH_UNCOMPRESSED_CODEC)
@@ -430,7 +430,7 @@ option(WITH_GDK_PIXBUF "Build gdk-pixbuf plugin" ON)
 
 option(WITH_REDUCED_VISIBILITY "Reduced symbol visibility in library" ON)
 
-option(WITH_DEFLATE_HEADER_COMPRESSION OFF)
+option(WITH_HEADER_COMPRESSION OFF)
 option(ENABLE_MULTITHREADING_SUPPORT "Switch off for platforms without multithreading support" ON)
 option(ENABLE_PARALLEL_TILE_DECODING "Will launch multiple decoders to decode tiles in parallel (requires ENABLE_MULTITHREADING_SUPPORT)" ON)
 

appveyor.yml
@@ -26,7 +26,7 @@ install:
 before_build:
   - mkdir build
   - cd build
-  - cmake .. -A %arch% -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DWITH_DAV1D=ON -DWITH_AOM_DECODER=ON -DWITH_AOM_ENCODER=ON -DWITH_JPEG_DECODER=ON -DWITH_JPEG_ENCODER=ON -DWITH_UNCOMPRESSED_CODEC=ON -DWITH_DEFLATE_HEADER_COMPRESSION=ON -DWITH_FFMPEG_DECODER=ON
+  - cmake .. -A %arch% -DCMAKE_TOOLCHAIN_FILE=c:/tools/vcpkg/scripts/buildsystems/vcpkg.cmake -DWITH_DAV1D=ON -DWITH_AOM_DECODER=ON -DWITH_AOM_ENCODER=ON -DWITH_JPEG_DECODER=ON -DWITH_JPEG_ENCODER=ON -DWITH_UNCOMPRESSED_CODEC=ON -DWITH_HEADER_COMPRESSION=ON -DWITH_FFMPEG_DECODER=ON
 
 build:
   verbosity: normal

examples/CMakeLists.txt
@@ -55,6 +55,10 @@ target_link_libraries(heif-enc PRIVATE heif)
 install(TARGETS heif-enc RUNTIME DESTINATION ${CMAKE_INSTALL_BINDIR})
 install(FILES heif-enc.1 DESTINATION ${CMAKE_INSTALL_MANDIR}/man1)
 
+if (WITH_HEADER_COMPRESSION)
+    target_compile_definitions(heif-enc PRIVATE WITH_HEADER_COMPRESSION=1)
+endif ()
+
 
 add_executable(heif-test ${getopt_sources}
         heif_test.cc

examples/heif_enc.cc
@@ -185,7 +185,9 @@ void show_help(const char* argv0)
             << "  --full_range_flag         nclx profile: full range flag, default: 1\n"
             << "  --enable-two-colr-boxes   will write both an ICC and an nclx color profile if both are present\n"
             << "  --premultiplied-alpha     input image has premultiplied alpha\n"
+#if WITH_HEADER_COMPRESSION
             << "  --enable-metadata-compression   enable XMP metadata compression (experimental)\n"
+#endif
             << "  -C,--chroma-downsampling ALGO   force chroma downsampling algorithm (nn = nearest-neighbor / average / sharp-yuv)\n"
             << "                                  (sharp-yuv makes edges look sharper when using YUV420 with bilinear chroma upsampling)\n"
             << "  --benchmark               measure encoding time, PSNR, and output file size\n"

libheif/CMakeLists.txt
@@ -134,14 +134,10 @@ else ()
     message("Not compiling 'libsharpyuv'")
 endif ()
 
-if (WITH_DEFLATE_HEADER_COMPRESSION OR WITH_UNCOMPRESSED_CODEC)
+if (WITH_HEADER_COMPRESSION OR WITH_UNCOMPRESSED_CODEC)
     find_package(ZLIB REQUIRED)
     target_link_libraries(heif PRIVATE ZLIB::ZLIB)
     target_compile_definitions(heif PRIVATE HAVE_ZLIB=1)
-endif ()
-
-if (WITH_DEFLATE_HEADER_COMPRESSION)
-    target_compile_definitions(heif PRIVATE WITH_DEFLATE_HEADER_COMPRESSION=1)
 endif ()
 
 if (HAVE_BROTLI)

libheif/compression.h
@@ -29,15 +29,17 @@
 #if HAVE_ZLIB
 std::vector<uint8_t> compress_zlib(const uint8_t* input, size_t size);
 
-Error decompress_zlib(const std::vector<uint8_t>&compressed_input, std::vector<uint8_t> *output);
+std::vector<uint8_t> compress_deflate(const uint8_t* input, size_t size);
 
-Error decompress_deflate(const std::vector<uint8_t>& compressed_input, std::vector<uint8_t> *output);
+Error decompress_zlib(const std::vector<uint8_t>& compressed_input, std::vector<uint8_t>* output);
+
+Error decompress_deflate(const std::vector<uint8_t>& compressed_input, std::vector<uint8_t>* output);
 
 #endif
 
 #if HAVE_BROTLI
 
-Error decompress_brotli(const std::vector<uint8_t> &compressed_input, std::vector<uint8_t> *output);
+Error decompress_brotli(const std::vector<uint8_t>& compressed_input, std::vector<uint8_t>* output);
 
 #endif
 

libheif/compression_zlib.cc
@@ -28,7 +28,7 @@
 #include <cstring>
 #include <iostream>
 
-std::vector<uint8_t> compress_zlib(const uint8_t* input, size_t size)
+std::vector<uint8_t> compress(const uint8_t* input, size_t size, int windowSize)
 {
   std::vector<uint8_t> output;
 
@@ -50,7 +50,7 @@ std::vector<uint8_t> compress_zlib(const uint8_t* input, size_t size)
   strm.zfree = Z_NULL;
   strm.opaque = Z_NULL;
 
-  int err = deflateInit(&strm, Z_DEFAULT_COMPRESSION);
+  int err = deflateInit2(&strm, Z_DEFAULT_COMPRESSION, Z_DEFLATED, windowSize, 8, Z_DEFAULT_STRATEGY);
   if (err != Z_OK) {
     return {}; // TODO: return error
   }
@@ -134,6 +134,17 @@ Error do_inflate(const std::vector<uint8_t>& compressed_input, int windowSize, s
 
   return Error::Ok;
 }
+
+std::vector<uint8_t> compress_zlib(const uint8_t* input, size_t size)
+{
+  return compress(input, size, 15);
+}
+
+std::vector<uint8_t> compress_deflate(const uint8_t* input, size_t size)
+{
+  return compress(input, size, -15);
+}
+
 
 Error decompress_zlib(const std::vector<uint8_t>& compressed_input, std::vector<uint8_t> *output)
 {

commit 593d1c1366932238a3fda42ebf9e1fb271f92c77

fix ABI break in enum heif_metadata_compression
This fixes the ABI break introduced in ebf6c61260307b0514c134232f79b006a8960b49 .

Resolves #1387 .

libheif/api/libheif/heif.h
@@ -519,8 +519,8 @@ enum heif_metadata_compression
 {
   heif_metadata_compression_off = 0,
   heif_metadata_compression_auto = 1,
-  heif_metadata_compression_unknown = 2, // only used when reading unknown method from input file
-  heif_metadata_compression_deflate = 3,
+  heif_metadata_compression_deflate = 2,
+  heif_metadata_compression_unknown = 3, // only used when reading unknown method from input file
   heif_metadata_compression_zlib = 4,    // do not use for header data
   heif_metadata_compression_brotli = 5
 };