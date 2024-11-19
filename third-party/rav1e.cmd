b2f86f6e33463c765e20cac7fcbda9f08f9f5d8e
Dirk Farin <dirk.farin@gmail.com>
2024-11-19 21:22:51 +0100

    update rav1e to v0.7.1

--- third-party/rav1e.cmd
+++ third-party/rav1e.cmd
@@ -1,7 +1,7 @@
 : # This install script was originally taken from libavif but might have been modified.
 
 : # If you want to enable the RAV1E encoder, please check that the WITH_RAV1E CMake variable is set correctly.
-: # You will also have to set the PKG_CONFIG_PATH to "third-party/rav1e/dist/lib/pkgconfig" so that the local RAV1E library is found.
+: # You will also have to set the PKG_CONFIG_PATH to "third-party/rav1e/dist/lib/x86_64-linux-gnu/pkgconfig" (depending on your architecture) so that the local RAV1E library is found.
 
 : # The odd choice of comment style in this file is to try to share this script between *nix and win32.
 
@@ -13,7 +13,7 @@
 : # Also, the error that "The target windows-msvc is not supported yet" can safely be ignored provided that rav1e/target/release
 : # contains rav1e.h and rav1e.lib.
 
-git clone -b 0.6 --depth 1 https://github.com/xiph/rav1e.git
+git clone -b v0.7.1 --depth 1 https://github.com/xiph/rav1e.git
 
 cd rav1e
 cargo install cargo-c
