b35485f1f29e521d61b6004f435efcfe93043b64
Victor Loh <vloh@fb.com>
2024-11-21 14:41:12 -0800

    remove unused header in decoder_libde265.h

--- libheif/plugins/decoder_libde265.h
+++ libheif/plugins/decoder_libde265.h
@@ -21,8 +21,6 @@
 #ifndef LIBHEIF_HEIF_DECODER_DE265_H
 #define LIBHEIF_HEIF_DECODER_DE265_H
 
-#include "common_utils.h"
-
 const struct heif_decoder_plugin* get_decoder_plugin_libde265();
 
 #if PLUGIN_LIBDE265
