aee81f0a629304ff730989abbff69ee4ab6c74d5
Dirk Farin <dirk.farin@gmail.com>
2025-04-30 17:47:03 +0200

    TAI: unit test checks reading image with TAI

--- tests/tai.cc
+++ tests/tai.cc
@@ -31,7 +31,9 @@
 
 TEST_CASE( "image-tai" )
 {
-  heif_error err;
+  heif_error err{};
+
+  std::string filename = get_tests_output_file_path("tai-1.heic");
 
   heif_image* img = createImage_RGB_planar();
   heif_encoder* enc = get_encoder_or_skip_test(heif_compression_HEVC);
@@ -53,6 +55,8 @@ TEST_CASE( "image-tai" )
   err = heif_item_set_property_tai_clock_info(ctx, itemId, clock_info, nullptr);
   REQUIRE(err.code == heif_error_Ok);
 
+  heif_tai_clock_info_release(clock_info);
+
   // add TAI timestamp
 
   heif_tai_timestamp_packet* timestamp = heif_tai_timestamp_packet_alloc();
@@ -64,13 +68,62 @@ TEST_CASE( "image-tai" )
   err = heif_item_set_property_tai_timestamp(ctx, itemId, timestamp, nullptr);
   REQUIRE(err.code == heif_error_Ok);
 
-  err = heif_context_write_to_file(ctx, get_tests_output_file_path("tai-1.heic").c_str());
+  heif_tai_timestamp_packet_release(timestamp);
+
+  err = heif_context_write_to_file(ctx, filename.c_str());
   REQUIRE(err.code == heif_error_Ok);
 
   heif_image_handle_release(handle);
   heif_context_free(ctx);
+  heif_image_release(img);
+
 
   // --- read file
 
+  ctx = heif_context_alloc();
+  err = heif_context_read_from_file(ctx, filename.c_str(), nullptr);
+  REQUIRE(err.code == heif_error_Ok);
 
+  err = heif_context_get_primary_image_handle(ctx, &handle);
+  REQUIRE(err.code == heif_error_Ok);
+
+  itemId = heif_image_handle_get_item_id(handle);
+
+  clock_info = nullptr; // make sure that we are not accidentally using old data
+  err = heif_item_get_property_tai_clock_info(ctx, itemId, &clock_info);
+  REQUIRE(err.code == heif_error_Ok);
+  REQUIRE(clock_info != nullptr);
+
+  timestamp = nullptr; // make sure that we are not accidentally using old data
+  err = heif_item_get_property_tai_timestamp(ctx, itemId, &timestamp);
+  REQUIRE(err.code == heif_error_Ok);
+  REQUIRE(timestamp != nullptr);
+
+  REQUIRE(clock_info->clock_resolution == 1000);
+  REQUIRE(clock_info->clock_drift_rate == 123);
+  REQUIRE(clock_info->clock_type == heif_tai_clock_info_clock_type_synchronized_to_atomic_source);
+  REQUIRE(clock_info->time_uncertainty == 999);
+  heif_tai_clock_info_release(clock_info);
+
+  REQUIRE(timestamp->tai_timestamp == 1234567890);
+  REQUIRE(timestamp->synchronization_state == 1);
+  REQUIRE(timestamp->timestamp_generation_failure == 0);
+  REQUIRE(timestamp->timestamp_is_modified == 0);
+  heif_tai_timestamp_packet_release(timestamp);
+
+  // check whether we can get the timestamp also from the decoded image
+
+  err = heif_decode_image(handle, &img, heif_colorspace_undefined, heif_chroma_undefined, nullptr);
+  REQUIRE(err.code == heif_error_Ok);
+
+  timestamp = nullptr; // make sure that we are not accidentally using old data
+  err = heif_image_get_tai_timestamp(img, &timestamp);
+  REQUIRE(err.code == heif_error_Ok);
+  REQUIRE(timestamp != nullptr);
+
+  REQUIRE(timestamp->tai_timestamp == 1234567890);
+  REQUIRE(timestamp->synchronization_state == 1);
+  REQUIRE(timestamp->timestamp_generation_failure == 0);
+  REQUIRE(timestamp->timestamp_is_modified == 0);
+  heif_tai_timestamp_packet_release(timestamp);
 }
