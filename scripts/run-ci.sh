aa67abfe4026f90cefd72ca7ceb151f4c9a1e419
Dirk Farin <dirk.farin@gmail.com>
2025-04-30 21:13:38 +0200

    CI: try to show stacktrace when program crashes

--- scripts/run-ci.sh
+++ scripts/run-ci.sh
@@ -174,7 +174,7 @@ if [ -z "$EMSCRIPTEN_VERSION" ] && [ -z "$CHECK_LICENSES" ] && [ -z "$TARBALL" ]
         ${BIN_WRAPPER} ./examples/heif-dec${BIN_SUFFIX} --list-decoders
 
         echo "Dumping information of sample file ..."
-        ${BIN_WRAPPER} ./examples/heif-info${BIN_SUFFIX} --dump-boxes examples/example.heic
+        ${BIN_WRAPPER} gdb -batch -ex "run" -ex "bt" --args ./examples/heif-info${BIN_SUFFIX} --dump-boxes examples/example.heic
         if [ ! -z "$WITH_GRAPHICS" ] && [ ! -z "$WITH_HEIF_DECODER" ]; then
             echo "Converting sample HEIF file to JPEG ..."
             ${BIN_WRAPPER} ./examples/heif-dec${BIN_SUFFIX} examples/example.heic example.jpg
