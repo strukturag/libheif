6f5a00784689817fee17e5b7b46c414dc58235ea
Dinika Saxena <dinika@greyllama.cc>
2025-10-20 18:19:22 +0200

    Improve documentation on how to use SVT as a builtin encoder
    Signed-off-by: Dinika Saxena <dinika@greyllama.cc>

--- third-party/svt.cmd
+++ third-party/svt.cmd
@@ -2,7 +2,7 @@
 
 : # cmake and ninja must be in your PATH for compiling.
 
-: # If you want to enable the SVT-AV1 encoder, please check that the WITH_SvtEnc and WITH_SvtEnc_BUILTIN CMake variables are set correctly.
+: # If you want to enable the SVT-AV1 encoder, please check that the WITH_SvtEnc and WITH_SvtEnc_PLUGIN CMake variables have correct values.
 : # You will also have to set the PKG_CONFIG_PATH to "third-party/SVT-AV1/Build/linux/Release" so that the local SVT-AV1 library is found.
 
 git clone -b v3.0.0 --depth 1 https://gitlab.com/AOMediaCodec/SVT-AV1.git
