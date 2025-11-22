55dfcee7ec6c4d6bc356b63094c92f3aca176e05
Dirk Farin <dirk.farin@gmail.com>
2025-11-22 21:27:25 +0100

    [BSD3] svt encoder plugin: cleanup

--- third-party/svt.cmd
+++ third-party/svt.cmd
@@ -5,7 +5,7 @@
 : # If you want to enable the SVT-AV1 encoder, please check that the WITH_SvtEnc and WITH_SvtEnc_PLUGIN CMake variables have correct values.
 : # You will also have to set the PKG_CONFIG_PATH to "third-party/SVT-AV1/Build/linux/Release" so that the local SVT-AV1 library is found.
 
-git clone -b v3.0.0 --depth 1 https://gitlab.com/AOMediaCodec/SVT-AV1.git
+git clone -b v3.0.1 --depth 1 https://gitlab.com/AOMediaCodec/SVT-AV1.git
 
 cd SVT-AV1
 cd Build/linux
